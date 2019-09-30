99344ccec3d821c72ba5a055b2d6fc94543deced
jakewilliami <jakewilliami@icloud.com>
2019-10-01 10:07:19 +1300

    realised case doesn't matter too much

--- python/searchbycode_github.py
+++ python/searchbycode_github.py
@@ -5,6 +5,6 @@ parser = argparse.ArgumentParser(description='Process string as language')
 parser.add_argument('language', help="Your desired langauge to search by.")
 args = parser.parse_args()
 
-url_search = "https://github.com/search?q=language%3A"+args.language+""
+url_search = "https://github.com/search?q=language%3A"+args.language.upper()+""
 
 webbrowser.open(url_search) #Opens website
