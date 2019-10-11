0d37045fd92eea01fc1285cf07c3b527f19091c5
Jake Ireland <jakewilliami@icloud.com>
2019-10-11 20:22:06 +1300

    refined code; added print statements with logic in colour; added run comments

--- pdfsearches/pdfsearch.pl
+++ pdfsearches/pdfsearch.pl
@@ -1,43 +1,41 @@
+# Run using perl -X pdfsearches/pdfsearch.pl
+
 #!/usr/bin/perl
 
 ### WORK IN PROGRESS ###
 
-#forces us to write good code and show us comprehensive errors
-#use strict;
-#use warnings;
-#use diagnostics;
-
-#adds function to print line followed by a new line
+# adds function to print line followed by a new line
 use feature 'say';
 
-#for demonstration of switch (give and win) statement inside of perl
-use feature "switch";
-
-#for defining to use a specific version of Perl
+# for defining to use a specific version of Perl
 use v5.30;
 
-#install using `cpanm PDF::Core`
+# install using `cpanm lib::rary`
 use PDF::Core;
 use File::Extract::PDF;
 use CAM::PDF;
 use Text::FromAny;
+use Term::ANSIColor;
+use warnings;
 
-#cannot install
-#use CGI::Application::Search;
-#use PDF::OCR::Thorough
-
-########################################################################
+# This is a helper function to initialize our %mutc variable.  (https://github.com/evalEmpire/method-signatures/pull/129/commits/bbed93f169a74b94da67c08d1f5f9e2c39daf130)
+#no warnings 'deprecated';
+#require Any::Moose;
+#Any::Moose->import('::Util::TypeConstraints');
 
 my $filename = "/Users/jakeireland/Desktop/Study/Victoria University/2018/Trimester 2/PSYC221/Minority Report/Report/Minority Report.pdf";
 
 my $searchstring = "Allport";
- 
-#my $pdf = CAM::PDF->new($filename);
 
 my $tFromAny = Text::FromAny->new(
     file => $filename);
+    
 my $text = $tFromAny->text, "\n";
 
 my $grepped = grep($searchstring, $text);
 
-print($grepped);
+if (not defined $grepped) {
+    print color("red"), "Not found in ${filename}", color("reset");
+} else {
+    print color("green"), "Found in ${filename}!", color("reset");
+}
