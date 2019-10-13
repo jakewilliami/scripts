d129397c2527232d95880e2e874451c0e8c43282
Jake Ireland <jakewilliami@icloud.com>
2019-10-14 11:02:54 +1300

    Got ruby script working basically

--- pdfsearches/pdfsearch.pl
+++ pdfsearches/pdfsearch.pl
@@ -17,25 +17,32 @@ use CAM::PDF;
 use Text::FromAny;
 use Term::ANSIColor;
 use warnings;
+use strict;
 
 # This is a helper function to initialize our %mutc variable.  (https://github.com/evalEmpire/method-signatures/pull/129/commits/bbed93f169a74b94da67c08d1f5f9e2c39daf130)
 #no warnings 'deprecated';
 #require Any::Moose;
 #Any::Moose->import('::Util::TypeConstraints');
 
-my $filename = "/Users/jakeireland/Desktop/Study/Victoria University/2018/Trimester 2/PSYC221/Minority Report/Report/Minority Report.pdf";
+my $fileName = "/Users/jakeireland/Desktop/Study/Victoria University/2018/Trimester 2/PSYC221/Minority Report/Report/Minority Report.pdf";
+my $fileDir = "x";
+my $searchString = $ARGV[0];
 
-my $searchstring = "Allport";
+sub found {
+    print color("green"), "\"${searchString}\" found in ${fileName}!", color("reset");
+}
+sub notFound {
+    print color("red"), "\"${searchString}\" not found in ${fileName}", color("reset");
+}
+
 
 my $tFromAny = Text::FromAny->new(
-    file => $filename);
+    file => $fileName);
     
-my $text = $tFromAny->text, "\n";
+my $text = $tFromAny->text;
 
-my $grepped = grep($searchstring, $text);
-
-if (not defined $grepped) {
-    print color("red"), "Not found in ${filename}", color("reset");
+if (index($text, $searchString) != -1) {
+   found();
 } else {
-    print color("green"), "Found in ${filename}!", color("reset");
+    notFound();
 }
--- pdfsearches/pdfsearch.rb
+++ pdfsearches/pdfsearch.rb
@@ -1,31 +1,18 @@
 #!/usr/bin/ruby
 
-#For rails to work, you need to run 
-#sudo gem install rails
-#which requires ruby 2.4.4 and above.  So run the following lines, which installs Ruby 2.6.4 and sets it as your default Ruby version
-#brew install ruby-build
-#brew install rbenv
-#rbenv install 2.6.4
-#rbenv global 2.6.4
-
 #gem install pdf-reader if out of date
 require 'pdf-reader'
 require 'rubygems'
-require 'pdf/reader'
 require 'colorize'
 require 'pathname'
-#require 'rails/engine'
+require 'rails'
+require "find"
 
 #puts 'What is your name?'
 #name = gets.chomp
 #puts "Hello #{name}!"
 
-#name = ARGV[0]
-#puts "Hello #{name}!"
-
 ##########################################################
-
-#coding: utf-8
 
 class String
 def bold;           "\033[1m#{self}"end
@@ -35,64 +22,18 @@ def blink;          "\033[5m#{self}" end
 def reverse_color;  "\033[7m#{self}" end
 end
 
-def os_walk(dir)
-  root = Pathname(dir)
-  files, dirs = [], []
-  Pathname(root).find do |path|
-    unless path == root
-      dirs << path if path.directory?
-      files << path if path.file?
-    end
-  end
-  [root, files, dirs]
-end
-
 fileName = '/Users/jakeireland/Desktop/Study/Victoria University/2018/Trimester 2/PSYC221/Minority Report/Report/Minority Report.pdf'
 
 mainDir = '/Users/jakeireland/Desktop/Study/Victoria University/2018/Trimester 2/PSYC221/'
 
 stringName = ARGV[0]
 
-root, files, dirs = os_walk(mainDir)
-
-#for f in os_walk(mainDir) do
-#    if File.extname("#{f}") == ".pdf"
-#        puts "#{f}"
-#    else
-#        puts "nothing"
-#    end  
-#end
-
-#reader = PDF::Reader.new('/Users/jakeireland/Desktop/Study/Victoria University/2018/Trimester 2/PSYC221/Minority Report/Report/Minority Report.pdf')
 
 found = false
+os_walk = Dir.glob( "#{mainDir}/**/*.pdf" )
 
-##for f in os_walk(mainDir) do
-#Dir[ './*.pdf' ].select{ |f| File.file? f } 
-##    Dir.glob('*.pdf').each do |f|
-#    #  fileName = Dir.glob('*.pdf')
-#    #  File.open(fileName, 'r') do |file|
-#        reader = PDF::Reader.new('#{f}')
-#        PDF::Reader.open(f) do |reader|
-#        reader.pages.each do |page|
-#            plainText = page.text
-#            grepped = plainText.match(/#{stringName}/i)
-#            if grepped.nil?
-#
-#            else
-#                found = true
-#            end
-#        end
-##        end
-#    #  end
-#    end
-#end
-##end
-
-#for f in os_walk(mainDir) do
-#Dir[ 'mainDir/*.pdf' ].select{ |f| File.file? f } 
-    #  File.open(fileName, 'r') do |file|
-    reader = PDF::Reader.new(Rails.mainDir.join(f))
+for f in os_walk do
+    reader = PDF::Reader.new(f)
     PDF::Reader.open(f) do |reader|
     reader.pages.each do |page|
         plainText = page.text
@@ -101,17 +42,14 @@ found = false
 
         else
             found = true
+            if (found == true)
+                puts "String #{stringName} found in #{f}!".green
+            else
+                puts "String #{stringName} not found in #{f}".red
+            end
         end
     end
     end
-#end
-
-
-
-if (found == true)
-    puts "String #{stringName} found in #{fileName}!".green
-else
-    puts "String #{stringName} not found in #{fileName}".red
 end
 
 #puts reader.pdf_version
