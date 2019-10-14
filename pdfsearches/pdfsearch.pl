cb25601ba5dd17b2e7b9f79cf3b421525ffe3958
Jake Ireland <jakewilliami@icloud.com>
2019-10-15 12:42:27 +1300

    Made script look through files recursively

--- pdfsearches/pdfsearch.pl
+++ pdfsearches/pdfsearch.pl
@@ -4,9 +4,6 @@
 
 ### WORK IN PROGRESS ###
 
-# adds function to print line followed by a new line
-use feature 'say';
-
 # for defining to use a specific version of Perl
 use v5.30;
 
@@ -16,33 +13,31 @@ use File::Extract::PDF;
 use CAM::PDF;
 use Text::FromAny;
 use Term::ANSIColor;
-use warnings;
 use strict;
+use File::Find;
 
 # This is a helper function to initialize our %mutc variable.  (https://github.com/evalEmpire/method-signatures/pull/129/commits/bbed93f169a74b94da67c08d1f5f9e2c39daf130)
 #no warnings 'deprecated';
 #require Any::Moose;
 #Any::Moose->import('::Util::TypeConstraints');
 
-my $fileName = "/Users/jakeireland/Desktop/Study/Victoria University/2018/Trimester 2/PSYC221/Minority Report/Report/Minority Report.pdf";
-my $fileDir = "x";
+my $fileDir = "/Users/jakeireland/Desktop/Study/Victoria University/2018/Trimester 2/";#PSYC221/";
 my $searchString = $ARGV[0];
 
-sub found {
-    print color("green"), "\"${searchString}\" found in ${fileName}!", color("reset");
-}
-sub notFound {
-    print color("red"), "\"${searchString}\" not found in ${fileName}", color("reset");
+sub eachFile {
+  my $filename = $_;
+  my $fullpath = $File::Find::name;
+  foreach my $f ($filename) {
+        if ($f =~ /\.pdf\z/) {
+            my $tFromAny = Text::FromAny->new(
+            file => $f);
+            my $text = $tFromAny->text;
+            if (index($text, $searchString) != -1) {
+               print color("BOLD"), "\"${searchString}\" found in ${filename}!\n", color("reset");
+            } 
+        }
+    }
 }
 
+find (\&eachFile, $fileDir)
 
-my $tFromAny = Text::FromAny->new(
-    file => $fileName);
-    
-my $text = $tFromAny->text;
-
-if (index($text, $searchString) != -1) {
-   found();
-} else {
-    notFound();
-}
