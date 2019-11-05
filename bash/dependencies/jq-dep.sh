4b2551d3e65c6c460c8dd09ac18c475ef68804b8
Jake Ireland <jakewilliami@icloud.com>
2019-11-05 21:11:35 +1300

    Fixed typo not printing colour

--- bash/dependencies/jq-dep.sh
+++ bash/dependencies/jq-dep.sh
@@ -2,7 +2,7 @@
 
 if ! brew ls --versions jq > /dev/null
 then
-    echo -e "\001b[1;38mInstalling \`jq\`...\u001b[0;38m" && \
+    echo -e "\u001b[1;38mInstalling \`jq\`...\u001b[0;38m" && \
     brew install jq && \
     echo -e "\u001b[1;38;5;2m\`jq\` installed successfully\u001b[0;38m"
 fi
