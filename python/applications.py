21e6e3b47ceb7fb64380616f0efae99c31167654
Jake Ireland <jakewilliami@icloud.com>
2019-11-17 22:11:06 +1300

    began work on applications.py

--- python/applications.py
+++ python/applications.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python
 
+import plistlib  # for parsing the plist files
+import os  # for running the commands to generate lists
+from pathlib import Path  # for home dir
+
+
+# Set home
+home = str(Path.home())
+
+
+# Make text files of app data
+os.system("system_profiler -xml SPApplicationsDataType > ${HOME}/scripts/python/sysApps.txt")
+os.system("brew cask list > ${HOME}/scripts/python/casks.txt")
+    
+
+
+
+with open(home + "/scripts/python/sysApps.txt", 'rb') as f:
+    pl = load(f)
+#    print(pl)
+#print(pl["_name"])
+    
+#appname = pl['_name']
+#obtainedfrom = pl['obtained_from']
