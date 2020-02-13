dfa91d67bb9ba55162593404f3c2d51bcf5986e6
Jake W. Ireland <jakewilliami@icloud.com>
2020-02-14 11:01:54 +1300

    added r script

---
+++ r/test.R
@@ -0,0 +1,47 @@
+library(pacman)
+pacman::p_load(tidyjson,dplyr,tidyverse,jsonlite,rjson,dummies,tibble)
+
+# import dataset (untouched) as 'originalDataset' and dataset grepped using command line as 'testResponses'
+originalDataset <- readr::read_csv(file = "kfc_stimulusResponses.csv", col_names = TRUE)
+# testResponses <- readr::read_csv(file = "testResponses.csv", col_names = TRUE)
+
+# get index of user_ids that finished the questionnaire
+greppedByThanks <- originalDataset[grepl("Thanks for helping", originalDataset$data_string),]
+
+# create index of completed ids
+IDIndex <- greppedByThanks[[2]]
+
+# filter the original dataset by the index of completed IDs
+importantIDs <- filter(originalDataset, user_id %in% IDIndex)
+
+# this creates a dataframe of presponses made by participants who have finished
+testResponses <- importantIDs[grepl('"correct":(true|false)', importantIDs$data_string),]
+
+# sanity test: check number of user_ids completed and number of rows for the completed dataset
+length(IDIndex)
+nrow(importantIDs)
+
+
+# if parameter is empty, make an empty dataframe; else import it as JSON
+f <- function(.x) 
+  if (is.na(.x) || .x == "") data.frame()[1, ] else 
+    as.data.frame(jsonlite::fromJSON(.x))
+
+#name object called 'named' from dataframe and take the JSON out of it
+named <- testResponses %>% 
+  tidyr::unnest(data_string = lapply(data_string, f)) %>% 
+  mutate_at(vars("correct"), as.character)
+
+
+correct <- named[14]
+song_type <- named[7]
+song_type_str <- named[8]
+
+
+testResponses["correctness"] <- correct
+testResponses["song_type"] <- song_type
+testResponses["song_type_str"] <- song_type_str
+
+print(testResponses)
+
+
