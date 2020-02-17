e8ee19fd565b18c4f6795f47b799eae37995792e
Jake W. Ireland <jakewilliami@icloud.com>
2020-02-17 18:30:54 +1300

    added github linguist workaround

--- bash/dependencies/gem-install-dep.sh
+++ bash/dependencies/gem-install-dep.sh
@@ -2,7 +2,7 @@
 
 gem_install() {
     # Check dependencies
-    if ! gem list --quiet --installed "^${1}$"
+    if ! gem list --quiet --installed "^${1}$" > /dev/null 2>&1
     then
         echo -e "${BYELLOW} Installing dependencies...${NORM}" && \
         sudo gem install ${1} && \
