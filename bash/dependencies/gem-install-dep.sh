e0cbfcfa7bdcabb1e15c8403251e2840d2b118ef
Jake W. Ireland <jakewilliami@icloud.com>
2020-02-17 18:18:36 +1300

    updated gem list tags (silent outdated)

--- bash/dependencies/gem-install-dep.sh
+++ bash/dependencies/gem-install-dep.sh
@@ -2,7 +2,7 @@
 
 gem_install() {
     # Check dependencies
-    if ! gem list --silent -i "^${1}$"
+    if ! gem list --quiet --installed "^${1}$"
     then
         echo -e "${BYELLOW} Installing dependencies...${NORM}" && \
         sudo gem install ${1} && \
