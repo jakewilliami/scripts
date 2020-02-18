7332f9f555c3b29585e1ba7f5902c17ab2a81e5d
Jake W. Ireland <jakewilliami@icloud.com>
2020-02-18 20:03:46 +1300

    fix logic

--- bash/dependencies/is-command.sh
+++ bash/dependencies/is-command.sh
@@ -32,25 +32,27 @@ is-command-then-install() {
 }
 
 is-library-then-install() {
-#boolean for checking if we need to install commands
-    HAVE_DEPENDENCIES=false
+    #boolean for checking if we need to install commands
+    MISSING_DEPENDENCIES=false
+    DEPS_DOWNLOADED=false
     #echo satifying deps if needed
     for i in "${@}"
     do
         if ! $PACSEARCH | grep "${i}" > /dev/null 2>&1
         then
-            HAVE_DEPENDENCIES=true
+            MISSING_DEPENDENCIES=true
         fi
     done
     #echo satisfying deps if needed
-    $HAVE_DEPENDENCIES && \
+    $MISSING_DEPENDENCIES && \
     echo -e "${SATISFYING_LIBS}"
     #install deps if command is not found
     for i in "${@}"
     do
-        lib_install "${i}"
+        lib_install "${i}" && DEPS_DOWNLOADED=true
     done
-    #echo deps satisfied if needed
-    $HAVE_DEPENDENCIES && \
-    echo -e "${LIBS_SATISFIED}"
+    #echo libs satisfied if needed
+    $DEPS_DOWNLOADED && \
+    echo -e "${LIBS_SATISFIED}" || \
+    echo -e "${ERROR_OCCURRED}" 
 }
