1261367d55be78d7b7a228431dd80659af5e2ff5
Jake W. Ireland <jakewilliami@icloud.com>
2020-02-18 20:01:17 +1300

    fix logic

--- bash/dependencies/brew-install-dep.sh
+++ bash/dependencies/brew-install-dep.sh
@@ -30,10 +30,11 @@ lib_install() {
     PACINSTALL=false
     for i in "${@}"; #previously @:2
     do
-        if ! $PACSEARCH | grep "${i}" > /dev/null 2>&1
+        if ! $PACSEARCH | grep "${i}" > /dev/null 2>&1 # if can't find $i installed then install $i
         then
             echo -e "${ITWHITE}Installing ${1}${NORM}"
-            $PACMAN "${i}" && PACINSTALL=true
+            $PACMAN "${i}" && \
+            PACINSTALL=true
         else
             PACINSTALL=true
         fi
