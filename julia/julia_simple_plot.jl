20127bb0f9a4be262c3c2c44be50b93922e6ef8a
Jake W. Ireland <jakewilliami@icloud.com>
2020-02-29 19:44:57 +1300

    updated julia scripts; added tshark as a LIB dependency for fedora

--- julia/julia_simple_plot.jl
+++ julia/julia_simple_plot.jl
@@ -1,6 +1,7 @@
 #! /usr/bin/julia
 
-import Plots
+using Pkg
+Pkg.add("Plots")
 const pl = Plots # this create an an alias, equivalent to Python "import Plots as pl". Declaring it constant may improve the performances.
 pl.pyplot()
 pl.plot(rand(4,4))
