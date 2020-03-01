a7ad87e2ecfb86da18c10b895704e62bc506c2b8
Jake W. Ireland <jakewilliami@icloud.com>
2020-03-02 08:07:35 +1300

    renamed parent directory to fit naming conventions

---
+++ pdfsearches/pdfsearch.rs/src/main.rs
@@ -0,0 +1,21 @@
+// run using
+// cd /Users/jakeireland/scripts/pdfsearches/pdfsearchrs/src/ && cargo run main.rs
+
+#[macro_use]
+extern crate lopdf;
+
+// #[macro_use]
+// extern crate pdf_extract;
+// extern crate lopdf;
+
+use std::env;
+use std::path::PathBuf;
+use std::path;
+use std::io::BufWriter;
+use std::fs::File;
+// use pdf_extract::*;
+use lopdf::*;
+
+fn main() {
+    println!("Hello, world!");
+}
