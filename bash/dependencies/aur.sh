97a0270aac4b3f3e9b289b8485631e24dddc682c
Jake W. Ireland <jakewilliami@icloud.com>
2020-03-09 17:05:15 +1300

    pushed ids et al to /tmp/ instead of temp.d in macos-config for cross-system compat

--- bash/dependencies/aur.sh
+++ bash/dependencies/aur.sh
@@ -2,7 +2,7 @@
 
 aur_install() {
     URL="${2}"
-    if ! $PACSEARCH | grep "${1}" > /dev/null 2>&1
+    if ! $PACSEARCH | grep "^${1}$" > /dev/null 2>&1
     then
         cd "${HOME}"/Downloads/
         wget "${URL}" || echo -e "${ERROR_OCCURRED}"
