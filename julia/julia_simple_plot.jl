8d457bde0774b9f274136eaca84cd907184b5797
Jake W. Ireland <jakewilliami@icloud.com>
2020-03-13 11:37:15 +1300

    Added julia slope field

--- julia/julia_simple_plot.jl
+++ julia/julia_simple_plot.jl
@@ -1,7 +1,8 @@
 #! /usr/bin/julia
 
-using Pkg
-Pkg.add("Plots")
+# using Pkg
+# Pkg.add("Plots")
+using CalculusWithJulia
 const pl = Plots # this create an an alias, equivalent to Python "import Plots as pl". Declaring it constant may improve the performances.
 pl.pyplot()
 pl.plot(rand(4,4))
