47100539a686808119d320287dd6eca72b56a185
Jake W. Ireland <jakewilliami@icloud.com>
2020-05-19 07:41:27 +1200

    refactored julia scripts that have been deprecated due to python; updated derivative solver for partial derivatives

--- python/derivative-solver.py
+++ python/derivative-solver.py
@@ -1,13 +1,35 @@
 #! /usr/bin/env python3
 
 
+##### FOR MATH244; AUTUMN-WINTER, 2020
+
+
 from sympy import *
+import re # for prettifying the input equation
+
+
+# for multiple substitutions
+class Substitutable(str):
+  def __new__(cls, *args, **kwargs):
+    newobj = str.__new__(cls, *args, **kwargs)
+    newobj.sub = lambda fro,to: Substitutable(re.sub(fro, to, newobj))
+    return newobj
+
 
 # get derivatives of functions
 
 x = symbols('x')
+y = symbols('y')
 
-f = sin(x)
-d = diff(f)
+f = Substitutable("5*x*y**3")
+d1 = Substitutable(diff(f, x))
+d2 = Substitutable(diff(f, y))
 
-print(d)
+
+prettyFunction = f.sub("^yp = ", r"y' = ").sub(r"\*\*\((\d+)\)", r"^{\1}").sub(r"\*\*", r"^").sub(r"\*", r" · ").sub(r"/", r" ÷ ")
+prettyD1 = d1.sub("^yp = ", r"y' = ").sub(r"\*\*\((\d+)\)", r"^{\1}").sub(r"\*\*", r"^").sub(r"\*", r" · ").sub(r"/", r" ÷ ")
+prettyD2 = d2.sub("^yp = ", r"y' = ").sub(r"\*\*\((\d+)\)", r"^{\1}").sub(r"\*\*", r"^").sub(r"\*", r" · ").sub(r"/", r" ÷ ")
+
+print(r"Taking the derivative of {}".format(prettyFunction), "\n")
+print("\tWRT x:\t {}".format(prettyD1))
+print("\tWRT y:\t {}".format(prettyD2))
