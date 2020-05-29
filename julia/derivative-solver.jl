2ee2ede1a0d6c77d3b80ee34f92faf98778dce9c
Jake W. Ireland <jakewilliami@icloud.com>
2020-05-29 17:30:19 +1200

    updated derivative script and added manually computed hessian

--- julia/derivative-solver.jl
+++ julia/derivative-solver.jl
@@ -4,18 +4,18 @@
 # EXAMPLE for FINDING DERIVATIVES (MATH244; Autumn, 2020)
 # https://docs.sciml.ai/stable/basics/overview/#Overview-of-DifferentialEquations.jl-1
 # https://docs.sciml.ai/stable/tutorials/ode_example/#ode_example-1
+# https://mth229.github.io/symbolic.html
 #########################################################################################
 
-# f(x) = 2 - x^2; c = -0.75
-# sec_line(h) = x -> f(c) + (f(c + h) - f(c))/h * (x - c) # by the definition of a derivative
-# plot([f, sec_line(1), sec_line(.75), sec_line(.5), sec_line(.25)], -1, 1) # this graph shows f(x)=2−x2 at various secant lines at c=−0.75
 
-# import Pkg; Pkg.add("Calculus")
-# Previously tried using packages "CalculusWithJulia" and "DifferentialEquations"
-using Calculus
+using SymPy
 
-f = "5x^2"
+@vars x y
 
-d = differentiate(f, :x)
+f(x,y) = x^2 - 2*x + y^2 - 6*y + 12
 
-print(d)
+d = diff(f(x,y), x)
+i = integrate(f(x,y), x)
+
+println("First derivative with respect to x:\t", d)
+println("First integral with respect to x:\t", i)
