04d81606777a12b76c98634df44a70e60461c186
Jake W. Ireland <jakewilliami@icloud.com>
2020-06-12 00:17:41 +1200

    begain work (after long haitus) on pdfsearch rust addresses #21 (wow, that's an old issue...)

--- pdfsearches/pdfsearch.rs/src/main.rs
+++ pdfsearches/pdfsearch.rs/src/main.rs
@@ -15,36 +15,35 @@ use std::fs::File;
 use pdf_extract::*;
 use lopdf::*;
 
+fn getPDFText() {
+    //let output_kind = "html";
+    //let output_kind = "txt";
+    //let output_kind = "svg";
+    let file = env::args().nth(1).unwrap();
+    let output_kind = env::args().nth(2).unwrap_or_else(|| "txt".to_owned());
+    println!("{}", file);
+    let path = path::Path::new(&file);
+    let filename = path.file_name().expect("expected a filename");
+    let mut output_file = PathBuf::new();
+    output_file.push(filename);
+    output_file.set_extension(&output_kind);
+    let mut output_file = BufWriter::new(File::create(output_file).expect("could not create output"));
+    let doc = Document::load(path).unwrap();
+
+    print_metadata(&doc);
+
+    let mut output: Box<dyn OutputDev> = match output_kind.as_ref() {
+        "txt" => Box::new(PlainTextOutput::new(&mut output_file as &mut dyn std::io::Write)),
+        "html" => Box::new(HTMLOutput::new(&mut output_file)),
+        "svg" => Box::new(SVGOutput::new(&mut output_file)),
+        _ => panic!(),
+    };
+
+    output_doc(&doc, output.as_mut());
+}
+
 
 // https://github.com/jrmuizel/pdf-extract/blob/master/examples/extract.rs
 fn main() {
     println!("Hello, world!");
 }
-
-
-// fn main() {
-//     //let output_kind = "html";
-//     //let output_kind = "txt";
-//     //let output_kind = "svg";
-//     let file = env::args().nth(1).unwrap();
-//     let output_kind = env::args().nth(2).unwrap_or_else(|| "txt".to_owned());
-//     println!("{}", file);
-//     let path = path::Path::new(&file);
-//     let filename = path.file_name().expect("expected a filename");
-//     let mut output_file = PathBuf::new();
-//     output_file.push(filename);
-//     output_file.set_extension(&output_kind);
-//     let mut output_file = BufWriter::new(File::create(output_file).expect("could not create output"));
-//     let doc = Document::load(path).unwrap();
-//
-//     print_metadata(&doc);
-//
-//     let mut output: Box<dyn OutputDev> = match output_kind.as_ref() {
-//         "txt" => Box::new(PlainTextOutput::new(&mut output_file as &mut dyn std::io::Write)),
-//         "html" => Box::new(HTMLOutput::new(&mut output_file)),
-//         "svg" => Box::new(SVGOutput::new(&mut output_file)),
-//         _ => panic!(),
-//     };
-//
-//     output_doc(&doc, output.as_mut());
-// }
