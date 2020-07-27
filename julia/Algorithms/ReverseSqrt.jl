183b9c4a565214009b6005e02786f27b05bd2a2b
Jake W. Ireland <jakewilliami@icloud.com>
2020-07-28 00:36:45 +1200

    Added pitiful reverse sqrt script (tentatively addresses #151)

--- julia/Algorithms/ReverseSqrt.jl
+++ julia/Algorithms/ReverseSqrt.jl
@@ -11,3 +11,26 @@ AN IRRATIONAL FLOATING POINT VALUE (Autumn, 2020)
 
 
 # using Decimals # to help with floating point arithmetic
+# That is, given some integer, find the closest approximation of a square root, and find its remainder
+
+a = parse(Float64, ARGS[1])
+
+function main(a)
+	aSquared = a^2
+	if isinteger(aSquared)
+		return aSquared, 0
+	end
+	newaSquared = floor(aSquared)
+	while true
+		newaSquared = floor(newaSquared) - 1
+		if isinteger(sqrt(newaSquared))
+			remainder = rem(aSquared, floor(newaSquared))
+			return Integer(newaSquared), remainder
+			break
+		end
+	end
+end
+
+output = main(a)
+
+println(a, " = √", output[1], " with remainder ", output[2])
