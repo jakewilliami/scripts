0d3f0fd0232db15325a1baf61367707263003a30
Jake W. Ireland <jakewilliami@icloud.com>
2020-07-31 23:51:21 +1200

    added linear shift registers and fermat algorithm to find primes

---
+++ julia/Cryptography/LCG.jl
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+    #=
+    exec julia --project="~/scripts/julia/Cryptography/" "${BASH_SOURCE[0]}" "$@" -e 'include(popfirst!(ARGS))' \
+    "${BASH_SOURCE[0]}" "$@"
+    =#
+
+m = parse(Int, ARGS[1])
+a = parse(Int, ARGS[2])
+b = parse(Int, ARGS[3])
+x0 = parse(Int, ARGS[4])
+
+function lcgPeriod(m::Integer, a::Integer, b::Integer, x0::Integer)
+	sequenceVec = Vector()
+	sequenceVec = vcat(sequenceVec, x0)
+	global newx = x0
+
+	for i in 0:20
+		global newx = mod(a*newx + b, m)
+		sequenceVec = vcat(sequenceVec, newx)
+	end
+
+	return join(sequenceVec)
+end
+
+output = lcgPeriod(m, a, b, x0)
+
+println(output)
