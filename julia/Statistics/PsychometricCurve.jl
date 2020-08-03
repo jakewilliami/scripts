666ea8f36f1e62086993150a7ff80bea4fab2264
Jake W. Ireland <jakewilliami@icloud.com>
2020-08-04 08:15:30 +1200

    stats plotting psychometric curve

---
+++ julia/Statistics/PsychometricCurve.jl
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+    #=
+    exec julia --project="~/scripts/julia/Statistics/" "${BASH_SOURCE[0]}" "$@" -e 'include(popfirst!(ARGS))' \
+    "${BASH_SOURCE[0]}" "$@"
+    =#
+
+using DataFrames
+using CSV
+using FreqTables
+using Distributions
+
+function main()
+	dfRaw = DataFrame!(CSV.File("data.csv"))
+	dfPivot = freqtable(dfRaw, :Response, :Condition)
+
+	numberPresent = count(x -> isequal(x,"Present"), dfRaw[!, 1])
+	numberAbsent = count(x -> isequal(x,"Absent"), dfRaw[!, 1])
+
+	cdf(d::UnivariateDistribution, x::Real)
+
+	presentRate = dfPivot[:,2] / numberPresent
+	absentRate = dfPivot[:,1] / numberAbsent
+
+	return (dprimeYesNo(presentRate[2], absentRate[2]))
+end
+
+output = main()
+
+println(output)
