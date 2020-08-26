ebc65cfd1267596a423a7cb86176478af52d4cc2
Jake W. Ireland <jakewilliami@icloud.com>
2020-08-26 23:49:40 +1200

    updated url after microsoft took over

--- python/searchbycode_github.py
+++ python/searchbycode_github.py
@@ -4,9 +4,11 @@ import webbrowser
 import argparse
 
 parser = argparse.ArgumentParser(description='Process string as language')
+parser.add_argument('searchTerm', help="Your search term.")
 parser.add_argument('language', help="Your desired langauge to search by.")
 args = parser.parse_args()
 
-url_search = "https://github.com/search?q=language%3A"+args.language.upper()+""
+# url_search = "https://github.com/search?q=language%3A"+args.language.upper()+""
+url_search = "https://github.com/search?q=" + args.searchTerm.replace(" ", "+") + "+language%3A" + args.language + "&type=Repositories&ref=advsearch&l=" + args.language + "&l="
 
 webbrowser.open(url_search) #Opens website
