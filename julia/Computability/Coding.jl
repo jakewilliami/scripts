71cd8881f9d5d7c67801952587ea1d146f368db0
Jake W. Ireland <jakewilliami@icloud.com>
2020-09-05 22:15:53 +1200

    Began work on coding for computability; updated pairing and unpairing to be cleaner

--- julia/Computability/Coding.jl
+++ julia/Computability/Coding.jl
@@ -3,9 +3,14 @@
     exec julia --project="~/scripts/julia/Computability/" --color=yes --startup-file=no -e 'include(popfirst!(ARGS))' \
     "${BASH_SOURCE[0]}" "$@"
     =#
+    
+module Coding
 
-PairNTuple(x::Integer, y::Integer)::Integer = x < 0 || y < 0 ? error("We have only defined this function for natural numbers.  Use IntToNat!.") : Int(x + binomial(x+y+1, 2))
-PairNTuple(x::Integer, y::Integer, z::Integer...)::Integer = PairNTuple(x, PairNTuple(y, z...))::Integer
+export PairNTuple, NatToPair,
+        IntToNat, IntToNat!, NatToInt, NatToInt!, f
+
+PairNTuple(x::Integer, y::Integer)::Integer = x < 0 || y < 0 ? error("This function is only defined for natural numbers.  Use IntToNat!.") : BigInt(big(x) + binomial(big(x)+big(y)+1, 2))
+PairNTuple(x::Integer, y::Integer, z::Integer...)::Integer = PairNTuple(x, PairNTuple(y, z...)...) # multiple inputs
 
 function NatToPair(n::Number)
     a = nothing
@@ -20,36 +25,39 @@ function NatToPair(n::Number)
     end
 end
 
-IntToNat(z::Integer)::Integer = z >= 0 ? 2 * z : (2 * abs(z)) - 1
-IntToNat!(zs) = IntToNat.(zs)
+IntToNat(z::Integer)::Integer = z >= 0 ? 2 * z : (2 * abs(z)) - 1 # base case
+IntToNat(z::Integer, w::Integer...) = IntToNat(z), IntToNat(w...)... # multiple inputs
+IntToNat(r::Tuple{Integer,Integer})::Integer = PairNTuple(IntToNat!(r)...) # ℤ^2 ⟼ ℕ (integer pair to nat)
+
+IntToNat!(z::Integer, w::Integer...) = error("Instead of IntToNat!(integer1, integer2, ...) please use IntToNat.")
+IntToNat!(zs::Tuple{Integer,Integer}) = IntToNat.(zs)
+IntToNat!(zs::Vector{<:Integer}) = IntToNat.(zs)
 
 NatToInt(n) = n < 0 ? error("We have only defined this function for natural numbers.  Why are you even using it?") : iseven(n) ? Int(n / 2) : -Int(floor(n / 2) + 1)
 NatToInt!(ns) = NatToInt.(ns)
 
-IntPairToNat(r::Tuple{Integer,Integer})::Integer = PairNTuple(IntToNat!(r)...)
-
-
-
-
-
-
-
-
-
+end # end module
 
 
 ## Testing
 
+using Test: @test
+using .Coding
+
 function test()
-    println(PairNTuple(5,7) == 83)
-    println(PairNTuple(5,7,20) == 76250)
-    println(NatToPair(83) == (5,7))
-    println(IntToNat!([0,-1,1,-2,2,-3,3,-4,4]) == [0, 1, 2, 3, 4, 5, 6, 7, 8])
-    println(IntPairToNat((-1,2)) == 16)
-    println(IntPairToNat((1,1)) == 12)
-    println(NatToInt!([0, 1, 2, 3, 4, 5, 6, 7, 8]) == [0, -1, 1, -2, 2, -3, 3, -4, 4])
-    println(IntToNat(NatToInt(79)) == 79)
-    println(NatToInt(IntToNat(-40)) == -40)
+    @test PairNTuple(5,7) == 83
+    @test PairNTuple(5,7,20) == 76250
+    @test PairNTuple([1,2,3,4,5,6,7,8,9]...) == 6311822920396919125408612435408496798197806713267032880607810866415866365846756729940810210974488939507829451263691704142949582073486360979796574028521007676991056360811439992631142153411030118420604722583844533351520010617917043983043649066080781
+    @test NatToPair(83) == (5,7)
+    
+    @test IntToNat!([0,-1,1,-2,2,-3,3,-4,4]) == [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    @test IntToNat((-1,2)) == 16
+    @test IntToNat((1,1)) == 12
+    @test IntToNat(3,4) == (6, 8)
+    
+    @test NatToInt!([0, 1, 2, 3, 4, 5, 6, 7, 8]) == [0, -1, 1, -2, 2, -3, 3, -4, 4]
+    @test IntToNat(NatToInt(79)) == 79
+    @test NatToInt(IntToNat(-40)) == -40
 end
 
-# test()
+@time test()
--- julia/Computability/PairingFunction.jl
+++ julia/Computability/PairingFunction.jl
@@ -10,32 +10,15 @@ e.g.
 	./PairingFunction.jl 9 5 2 0
 =#
 
-# m = parse(Int, ARGS[1])
-# a = parse(Int, ARGS[2])
-# b = parse(Int, ARGS[3])
-# x0 = parse(Int, ARGS[4])
-
-a = parse(BigInt, ARGS[1])
-
-b = nothing
-c = nothing
-
-# parse any arguments extra if given
-if isequal(length(ARGS), 2)
-    b = parse(BigInt, ARGS[2])
-end
-if isequal(length(ARGS), 3)
-    b = parse(BigInt, ARGS[2])
-    c = parse(BigInt, ARGS[3])
-end
+input = [parse(BigInt, a) for a in ARGS]
     
 
-function pairTuple(x::Number, y::Number)
+function pairTuple(x::Integer, y::Integer)::Integer
     if x < 0 || y < 0
         error("We have only defined this function for natural numbers.  See Coding.jl.")
     end
     
-    z = Int(x + binomial(x+y+1, 2))
+    z = BigInt(big(x) + binomial(big(x)+big(y)+1, 2))
     # z = Int(x + (((x+y)*(x+y+1)) / 2)) # also from notes
     # z = Int(((1/2) * (x+y) * (x+y+1)) + y) # from http://en.wikipedia.org/wiki/Pairing_function#Cantor_pairing_function
     #https://docs.julialang.org/en/v1/manual/faq/#...-combines-many-arguments-into-one-argument-in-function-definitions
@@ -43,16 +26,9 @@ function pairTuple(x::Number, y::Number)
     return z
 end
 
-pairTuple(x::Number, y::Number, z::Number...) = pairTuple(x, pairTuple(y, z...))
-    
+pairTuple(x::Integer, y::Integer, z::Integer...)::Integer = pairTuple(x, pairTuple(y, z...))
 
-# get output
-if isequal(length(ARGS), 2)
-    paired = pairTuple(a, b)
-    println("We pair the input:")
-    println("\t< $a, $b > ⟼  $paired\n")
-elseif isequal(length(ARGS), 3)
-    paired = pairTuple(a, pairTuple(b, c))
-    println("We pair the input:")
-    println("\t< $a, $b, $c > ⟼  $paired\n")
-end
+
+
+paired = pairTuple(input...)
+println("\t< ", join(input, ", "), " > ⟼  ", paired)
