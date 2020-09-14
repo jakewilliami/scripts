c38af4e7a7c1a48df014fcdbc25c07e7e9110c90
Jake W. Ireland <jakewilliami@icloud.com>
2020-09-14 16:13:26 +1200

    Updated unpairing function to be more general; add output of pairing to be bigint by default (caveat: slower)

--- julia/Computability/PairingFunction.jl
+++ julia/Computability/PairingFunction.jl
@@ -13,20 +13,20 @@ e.g.
 input = [parse(BigInt, a) for a in ARGS]
     
 
-function pairTuple(x::Integer, y::Integer)::Integer
+function pairTuple(x::Integer, y::Integer)::BigInt
     if x < 0 || y < 0
         error("We have only defined this function for natural numbers.  See Coding.jl.")
     end
     
     z = BigInt(big(x) + binomial(big(x)+big(y)+1, 2))
-    # z = Int(x + (((x+y)*(x+y+1)) / 2)) # also from notes
-    # z = Int(((1/2) * (x+y) * (x+y+1)) + y) # from http://en.wikipedia.org/wiki/Pairing_function#Cantor_pairing_function
+    # z = BigInt(x + (((x+y)*(x+y+1)) / 2)) # also from notes
+    # z = BigInt(((1/2) * (x+y) * (x+y+1)) + y) # from http://en.wikipedia.org/wiki/Pairing_function#Cantor_pairing_function
     #https://docs.julialang.org/en/v1/manual/faq/#...-combines-many-arguments-into-one-argument-in-function-definitions
         
     return z
 end
 
-pairTuple(x::Integer, y::Integer, z::Integer...)::Integer = pairTuple(x, pairTuple(y, z...))
+pairTuple(x::Integer, y::Integer, z::Integer...)::BigInt = pairTuple(x, pairTuple(y, z...))
 
 
 
--- julia/Computability/UnpairingFunction.jl
+++ julia/Computability/UnpairingFunction.jl
@@ -12,27 +12,20 @@ e.g.
 
 ## TODO: Make depairing thruple more efficient
 
-n = parse(BigInt, ARGS[1])
-a = parse(BigInt, ARGS[2])
+# n = parse(Int, ARGS[1])
+# m = parse(BigInt, ARGS[2])
+using StatsPlots#Plots
+using CSV
+using DataFrames
+using Distributed
 
-
-pairTuple(x::Integer, y::Integer)::Integer = Int(x + binomial(x+y+1, 2))
-pairTuple(x::Integer, y::Integer, z::Integer...)::Integer = pairTuple(x, pairTuple(y, z...))
-
-
-function unzipTuple(z::Number)
-    """
-    Inverse of Cantor pairing function
-    http://en.wikipedia.org/wiki/Pairing_function#Inverting_the_Cantor_pairing_function
-    """
-    
-    w = floor((sqrt(8 * z + 1) - 1)/2)
-    t = (w^2 + w) / 2
-    y = Int(z - t)
-    x = Int(w - y)
-    
-    return x, y
+function displaymatrix(M::AbstractArray)
+    return show(IOContext(stdout, :limit => true, :compact => true, :short => true), "text/plain", M); print("\n")
 end
+
+
+pairTuple(x::Integer, y::Integer)::BigInt = BigInt(x + binomial(x+y+1, 2))
+pairTuple(x::Integer, y::Integer, z::Integer...)::BigInt = pairTuple(x, pairTuple(y, z...))
 
 
 #=
@@ -45,83 +38,92 @@ end
         π0(m) = (μx ≤ m)(∃y ≤ m)m = < x, y >
     and so π0 (and similarly π1) is primitive recursive.
 =#
-function findTuple(m::Number)
-    x = nothing
-    y = nothing
-    
-    # double loop:
-    for x in 0:m, y in 0:m
-        if isequal(pairTuple(x, y), m)
-            return x,y
-        end
-    end
-end
-
-
-function findThruple(m::Number)
-    x = nothing
-    y = nothing
-    z = nothing
-    
-    # comprehension loop:
-    # [i*j for i in 0:m, j in 0:m]
-    
-    # triple loop:
-    for x in 0:m, y in 0:m, z in 0:m
-        if isequal(pairTuple(x, y, z), m)
-            return x, y, z
-        end
-    end
-end
-
-
-
-function findQuadruple(m::Number)
-    x = nothing
-    y = nothing
-    z = nothing
-        
-    for x in 0:m
-        for y in 0:m
-            for z in 0:m
-                for w in 0:m
-                    if isequal(pairTuple(x, y, z, w), m)
-                        return x, y, z, w
-                    end
-                end
+@generated function unpair_ntuple(::Val{n}, m::Integer) where {n}
+    quote
+        @inbounds @fastmath Base.Cartesian.@nloops $n i d -> 0:m begin
+            if isequal(pairTuple((Base.Cartesian.@ntuple $n i)...), m)
+                return Base.Cartesian.@ntuple $n i
             end
         end
     end
 end
+
+# ntuple_out = @time unpair_ntuple(Val(n), m)
+# println("\t$m ⟼  < $ntuple_out >")
+
+
+#=
+@dmipeck's genius, not mine
+=#
+function iterative_unpair_ntuple(dimension::Integer, max::Integer)
+    result::Array{Integer}=zeros(dimension)
     
-
-# println("For each input, we can depair:")
-# for i in ARGS
-#     numI = parse(BigInt, i)
-#     unpaired = findTuple(numI)
-#     println("\t$numI ⟼  < $unpaired >")
-# end
-# println("\n")
-#
-# println("For each input, we can de-thruple:")
-# for i in ARGS
-#     numI = parse(BigInt, i)
-#     unpaired = findThruple(numI)
-#     println("\t$numI ⟼  < $unpaired >")
-# end
-
-out = nothing
-
-if isone(n)
-    return a
-elseif isequal(n, 2)
-    out = findTuple(a)
-elseif isequal(n, 3)
-    out = findThruple(a)
-elseif isequal(n, 4)
-    out = findQuadruple(a)
-else
-    println("This is programme does not currently support extraction of paired quintuples or greater.  This is because the algorithm used to extract these is a searching function, and is very slow for large numbers or large n (e.g., n > 5).")
+    while pairTuple(result...) != max
+        #increment array
+        result[1] += 1
+        for i in 1:dimension
+            if result[i] > max
+                if i == dimension
+                    return nothing
+                else
+                    result[i+1] += 1
+                    result[i] = 0
+                end
+            else
+                break
+            end
+        end
+    end
+    
+    return result
 end
 
-println("\t$a ⟼  < $out >")
+
+# ntuple_out_iterative = @time iterative_unpair_ntuple(n, m)
+# println("\t$m ⟼  < $ntuple_out_iterative >")
+
+
+# write(joinpath(dirname(dirname(@__FILE__)), "data", "faceness-scores.csv"), DataFrame(hcat(face_names, df_faces, non_face_names, df_non_faces)), writeheader=false)
+
+function genPlot()
+    stop_n_at = 4
+    stop_m_at = 200
+    num_of_datapoints = 100
+    data = Matrix{Union{AbstractFloat,Integer}}(undef,num_of_datapoints,4)# needs 3 columns: n; m; bonk time; doov time
+    m_data = []
+
+    # for n in 2:stop_n_at
+    #     for m in 1:stop_m_at
+    #         bonks = @elapsed unpair_ntuple(Val(n), m)
+    #         doovs = @elapsed iterative_unpair_ntuple(n, m)
+    #         data[m,:] .= [n, m, bonks, doovs]
+    #     end
+    # end
+    
+    for i in 1:num_of_datapoints
+        n = rand(2:stop_n_at)
+        m = rand(1:stop_m_at)
+        bonks = @elapsed unpair_ntuple(Val(n), m)
+        doovs = @elapsed iterative_unpair_ntuple(n, m)
+        data[i,:] .= [n, m, bonks, doovs]
+    end
+
+    # make n and m integers
+    # data[1:2,:] .= convert(Integer, )
+    # data[1:2,:] = [parse(BigInt, a) for a in ARGS]
+    # data[:,1:2] .= convert.(Integer, data[:,1:2])
+    # df = DataFrame(n = data[:,1], m = [:,2], bonk = [:,3], doov = [:,4])
+
+    # displaymatrix(data)
+    
+    # x is n tuples with mean
+
+
+    gr() # set plot backend
+    theme(:solarized)
+    
+    plot = scatter(data[:,1], data[:,3:4], smooth = true, fontfamily = font("Times"), xlabel = "n", ylabel = "Time elapsed during calculation [seconds]", label = ["Bonk's" "Doov's"])#, xlims = (0, stop_n_at))
+    savefig(plot, joinpath(homedir(), "Desktop", "doov_v_bonk,n=$stop_n_at,m=$stop_m_at,i=$num_of_datapoints.pdf"))
+end
+
+genPlot()
