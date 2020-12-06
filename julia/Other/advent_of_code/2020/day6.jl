8c7db046c45c85c559b5737bd634b429ac14fd99
Jake W. Ireland <jakewilliami@icloud.com>
2020-12-07 12:14:51 +1300

    Removed splatting

--- julia/Other/advent_of_code/2020/day6.jl
+++ julia/Other/advent_of_code/2020/day6.jl
@@ -1,17 +1,27 @@
+function flattenall(A::AbstractArray)
+    while any(x -> typeof(x) <: AbstractArray, A)
+        A = collect(Base.Iterators.flatten(A))
+    end
+
+    return A
+end
+
+deepunique(A::AbstractArray) = unique(flattenall(A))
+
 function count_yes(datafile::String)
     data = Vector{Int}()
     
     open(datafile) do io
         while ! eof(io)
-            buffer = Vector{Char}()
+            buffer = Vector{Vector{Char}}()
             line = readline(io)
             
             while ! isempty(line)
-                push!(buffer, collect(line)...)
+                push!(buffer, collect(line))
                 line = readline(io)
             end
             
-            push!(data, length(unique(buffer)))
+            push!(data, length(deepunique(buffer)))
         end
     end
     
@@ -23,26 +33,16 @@ println(count_yes("data6.txt"))
 #=
 BenchmarkTools.Trial:
   memory estimate:  1.03 MiB
-  allocs estimate:  12934
+  allocs estimate:  12149
   --------------
-  minimum time:     1.904 ms (0.00% GC)
-  median time:      1.992 ms (0.00% GC)
-  mean time:        2.165 ms (3.08% GC)
-  maximum time:     7.280 ms (0.00% GC)
+  minimum time:     1.158 ms (0.00% GC)
+  median time:      1.185 ms (0.00% GC)
+  mean time:        1.303 ms (3.99% GC)
+  maximum time:     7.399 ms (80.83% GC)
   --------------
-  samples:          2309
+  samples:          3834
   evals/sample:     1
 =#
-
-function flattenall(A::AbstractArray)
-    while any(x -> typeof(x) <: AbstractArray, A)
-        A = collect(Base.Iterators.flatten(A))
-    end
-
-    return A
-end
-
-deepunique(A::AbstractArray) = unique(flattenall(A))
 
 function count_unanimous(datafile::String)
     data = Vector{Int}()
@@ -63,19 +63,19 @@ function count_unanimous(datafile::String)
 
     return sum(data)
 end
-
-println(count_unanimous("data6.txt"))
+using BenchmarkTools
+@benchmark (count_unanimous("data6.txt"))
 
 #=
 BenchmarkTools.Trial:
   memory estimate:  1.18 MiB
   allocs estimate:  15400
   --------------
-  minimum time:     1.692 ms (0.00% GC)
-  median time:      1.803 ms (0.00% GC)
-  mean time:        1.929 ms (3.92% GC)
-  maximum time:     5.670 ms (57.28% GC)
+  minimum time:     1.652 ms (0.00% GC)
+  median time:      1.697 ms (0.00% GC)
+  mean time:        1.843 ms (4.03% GC)
+  maximum time:     6.418 ms (72.92% GC)
   --------------
-  samples:          2591
+  samples:          2712
   evals/sample:     1
 =#
