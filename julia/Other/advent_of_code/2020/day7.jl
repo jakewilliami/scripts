3a0cfd883348c09ad4a93d75f52c8fb4c1096ffe
Jake W. Ireland <jakewilliami@icloud.com>
2020-12-08 23:56:38 +1300

    Cleanup and benchmarking results for day 7

--- julia/Other/advent_of_code/2020/day7.jl
+++ julia/Other/advent_of_code/2020/day7.jl
@@ -1,41 +1,41 @@
-function find_bag(
+function isbag_in_bag(
     dict::Dict,
     key::Union{String, SubString},
     search_value::Union{String, SubString}
     )
+    #= Doov's
+    bags = []
+    bags_added = []
     
+    while true
+        for outer_bag in keys(dict)
+            if key in dict[outer_bag] && outer_bag ∉ bags
+                push!(bags_added, outer_bag)
+            end
+        end
+        if isempty(bags_added)
+            break
+        else
+            push!(bags, bags_added)
+            bags_added = []
+        end
+    end
+    =#
+
     inner_bags = dict[key]
     
-    # bags = []
-    # bags_added = []
-    #
-    # while true
-    #     for outer_bag in keys(dict)
-    #         if key in dict[outer_bag] && outer_bag ∉ bags
-    #             push!(bags_added, outer_bag)
-    #         end
-    #     end
-    #     if isempty(bags_added)
-    #         break
-    #     else
-    #         push!(bags, bags_added)
-    #         bags_added = []
-    #     end
-    # end
-
-    
-    if dict[key] isa AbstractArray
+    if inner_bags isa AbstractArray
         if haskey(dict, key)
             if search_value ∈ inner_bags
                 return true
-            elseif search_value == key || "no other" ∈ inner_bags # technically we disqualify when search value is directly search key
+            elseif search_value == key || "no other" ∈ inner_bags
                 return false
             end
         end
     
         for v in inner_bags
             if haskey(dict, v)
-                if find_bag(dict, v, search_value)
+                if isbag_in_bag(dict, v, search_value)
                     return true
                 end
             end
@@ -60,45 +60,32 @@ function count_bag_containers(datafile::String, search_bag::String)
     end
     
     
-    return sum([find_bag(babushka_bags, outer_bag, search_bag) for outer_bag in keys(babushka_bags)])
+    return sum([isbag_in_bag(babushka_bags, outer_bag, search_bag) for outer_bag in keys(babushka_bags)])
 end
 
 println(count_bag_containers("data7.txt", "shiny gold"))
 
 #=
 BenchmarkTools.Trial:
-  memory estimate:  197.71 MiB
-  allocs estimate:  3803843
+  memory estimate:  151.30 MiB
+  allocs estimate:  2908237
   --------------
-  minimum time:     185.956 ms (4.24% GC)
-  median time:      190.825 ms (5.28% GC)
-  mean time:        190.982 ms (5.44% GC)
-  maximum time:     199.302 ms (5.05% GC)
+  minimum time:     179.104 ms (3.45% GC)
+  median time:      189.062 ms (4.22% GC)
+  mean time:        190.356 ms (4.44% GC)
+  maximum time:     229.057 ms (3.69% GC)
   --------------
   samples:          27
   evals/sample:     1
 =#
 
-function count_bags(dict::Dict, search_bag::Union{String, SubString})
-    inner_most = Vector{Dict}()
-    
-    for contents in values(dict)
-        if isempty(contents)
-            push!(inner_most, contents)
-        end
-    end
-    
-    if search_bag ∈ inner_most
-        return 0
+function count_bags_recursive(dict::Dict, search_bag::Union{String, SubString})
+    inner_most = Dict{Union{String, SubString}, Union{Vector, Dict}}[contents for contents in values(dict) if isempty(contents)]
+
+    if search_bag ∉ inner_most
+        return reduce(+, [dict[search_bag][bag] * (count_bags_recursive(dict, bag) + 1) for bag in keys(dict[search_bag])], init = 0)
     else
-        num_bags = 0
-        
-        for bag in keys(dict[search_bag])
-            num_bags += dict[search_bag][bag]
-            num_bags += dict[search_bag][bag] * (count_bags(dict, bag))
-        end
-        
-        return num_bags
+        return 0
     end
 end
 
@@ -108,25 +95,32 @@ function count_bags_required(datafile::String, search_bag::String)
     open(datafile) do io
         while ! eof(io)
             line = readline(io)
-            inner_dict = Dict{String, Int}()
-            
+
             outer_bag, inner_bags = split(line, "contain")
             outer_bag = strip.(replace.(outer_bag, r"(\bbag\b)|(\bbags\b)" => ""))
             inner_bags = strip.(replace.(filter!(e -> e ≠ "", split(inner_bags, r"[,|.]")), r"(\bbag\b)|(\bbags\b)" => ""))
             inner_match = match.(r"(\d+)\s(.+)", inner_bags)
             inner_bags = all(isnothing.(inner_match)) ? inner_bags : getfield.(inner_match, :captures)
-            
-            if "no other" ∉ inner_bags
-                for i in inner_bags
-                    push!(inner_dict, last(i) => parse(Int, first(i)))
-                end
-            end
 
-            push!(babushka_bags, outer_bag => inner_dict)
+            push!(babushka_bags, outer_bag => Dict{Union{String, SubString}, Int}(last(i) => parse(Int, first(i)) for i in inner_bags if "no other" ∉ inner_bags))
         end
     end
-    
-    return count_bags(babushka_bags, search_bag)
+
+    return count_bags_recursive(babushka_bags, search_bag)
 end
 
 println(count_bags_required("data7.txt", "shiny gold"))
+
+#=
+BenchmarkTools.Trial:
+  memory estimate:  4.86 MiB
+  allocs estimate:  114871
+  --------------
+  minimum time:     9.895 ms (0.00% GC)
+  median time:      12.261 ms (0.00% GC)
+  mean time:        13.236 ms (3.35% GC)
+  maximum time:     45.043 ms (0.00% GC)
+  --------------
+  samples:          378
+  evals/sample:     1
+=#
