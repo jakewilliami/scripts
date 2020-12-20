bd4fca20086bd60915447d53cf81487526341a4f
Jake W. Ireland <jakewilliami@icloud.com>
2020-12-20 19:34:11 +1300

    More failing at doing this day

--- julia/Other/advent_of_code/2020/day14.jl
+++ julia/Other/advent_of_code/2020/day14.jl
@@ -16,7 +16,7 @@ apply_mask(mask::Vector{Int}, mem::Vector{Int}) = apply_mask!(copy(mask), mem)
 
 function part1(datafile::String)
     mem, mask = Vector{Int}(undef, 36), Vector{Int}(undef, 36)
-    mem, mask = zeros(Int, 36), zeros(Int, 36)
+    # mem, mask = zeros(Int, 36), zeros(Int, 36)
     # mem, mask = string(), string()
 
     open(datafile) do io
@@ -26,21 +26,26 @@ function part1(datafile::String)
             
             if identifier == "mask"
                 # apply_mask!(mask, mem)
-                # parse(Int, replace(mask, 'X' => '0'), base = 2)
+                mask = parse.(Int, replace(value, 'X' => '0'), base = 2)
+                # mask = parse.(Int, string.(collect(string(parse(Int, identifier), base = 2))))
+                # mask = parse.(Int, string.(collect(value)), base = 2)
                 # continue
             else
-                local_mem = Vector{Int}(undef, 36)
-                local_mem = zeros(Int, 36)
-                mem_addr = parse.(Int, string.(collect(string(parse(Int, match(r"\d", identifier).match), base = 2))))
-                mem_slot = parse(Int, value)
-                local_mem[(36 - length(mem_addr) + 1):end] .= mem_addr
+                # local_mem = Vector{Int}(undef, 36)
+                # local_mem = zeros(Int, 36)
+                # mem = parse.(Int, string.(collect(string(parse(Int, match(r"\d", identifier).match), base = 2))))
+                # mem = parse.(Int, string.(collect(match(r"\d", identifier).match)), base = 2)
+                println(mem)
+                # println(mem_addr)
+                # mem_slot = parse(Int, value)
+                # local_mem[(36 - length(mem_addr) + 1):end] .= mem_addr
                 # println("Applying mask to $local_mem")
                 # println(SubString.(line, findall(r"\d+", line)))
                 # println(parse(Int, match(r"\d", identifier).match))
-                println(parse(Int, value, base = 2))
+                # println(parse(Int, value, base = 2))
                 # println("hello")
-                apply_mask!(local_mem, mask)
-                mem = local_mem
+                # apply_mask!(local_mem, mask)
+                # mem = local_mem
                 # println("mask applied: $mem")
             end
         end
