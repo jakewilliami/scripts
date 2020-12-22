289093300d2997f88f741f50900f5030a0880d86
Jake W. Ireland <jakewilliami@icloud.com>
2020-12-22 19:14:42 +1300

    Tentatively finished part 2...but slow!

--- julia/Other/advent_of_code/2020/day14.jl
+++ julia/Other/advent_of_code/2020/day14.jl
@@ -2,28 +2,16 @@ using DelimitedFiles: readdlm
 
 const datafile = joinpath(@__DIR__, "inputs", "data14.txt")
 
-function pushto!(V::Vector{T}, val::T, i::Int) where T <: Number
-    if i > length(V)
-        [push!(V, zero(T)) for _ in (length(V) + 1):(i - 1)]
-        push!(V, val)
-    else
-        V[i] = val
-    end
-    
-    return V
-end
-
 function apply_mask(mem::Int, mask::AbstractString)
-    A, mask = collect(bitstring(mem)[end-35:end]), collect(mask)
+    A, mask = collect(bitstring(mem)[(end - 35):end]), collect(mask)
     i = findall(!isequal('X'), mask)
     A[i] .= mask[i]
     
     return parse(Int, join(A), base = 2)
 end
 
-function part1(datafile::String)
-    out = Dict{Int, Int}()
-    mask = string()
+function sum_masked_values(datafile::String)
+    out, mask = Dict{Int, Int}(), string()
 
     open(datafile) do io
         while ! eof(io)
@@ -42,27 +30,95 @@ function part1(datafile::String)
     return sum(values(out))
 end
 
-println(part1(datafile))
+println(sum_masked_values(datafile))
 
-# using Combinatorics
-#
-# function part2(input_file::String)
-#     storage = Dict()
-#
-#     for line in eachline(input_file)
-#         line[1:4] == "mask" && (global mask = line[8:end]) != 0 && continue
-#         store = SubString.(line, findall(r"\d+", line))
-#         masked = parse(Int, store[1]) | parse(Int, replace(mask, 'X' => '0'), base=2)
-#         masked &= parse(Int, replace(replace(mask, '0' => '1'), 'X' => '0'), base=2)
-#         possible = powerset(findall(x -> x == 'X', mask))
-#
-#         for mas in possible
-#             num = masked | parse(Int, join([(bit ∈ mas ? '1' : '0') for bit in 1:36]), base=2)
-#             storage[num] = parse(Int, store[2])
-#         end
-#     end
-#
-#     return sum(values(storage))
-# end
-#
-# println(part2("inputs/test.txt"))
+#=
+BenchmarkTools.Trial:
+  memory estimate:  1.37 MiB
+  allocs estimate:  16580
+  --------------
+  minimum time:     1.883 ms (0.00% GC)
+  median time:      2.397 ms (0.00% GC)
+  mean time:        3.276 ms (5.20% GC)
+  maximum time:     66.226 ms (0.00% GC)
+  --------------
+  samples:          1524
+  evals/sample:     1
+=#
+
+function unfold(A::Union{AbstractArray, Tuple})
+    while any(x -> typeof(x) <: Union{AbstractArray, Tuple}, A)
+        A = collect(Base.Iterators.flatten(A))
+    end
+
+    return A
+end
+
+function get_combinations(A::Vector{Char})
+    out = Vector{String}()
+
+    for t in (unfold.(reduce(Base.Iterators.product, [['0', '1'] for _ in 1:count(isequal('X'), A)])))
+        local_A = copy(A)
+        local_A[findall(isequal('X'), A)] .= t
+        push!(out, join(local_A))
+    end
+
+    return parse.(Int, out, base = 2)
+end
+
+function get_combinations(A::AbstractString)
+    A, out = A isa AbstractString ? collect(A) : A, Vector{String}()
+
+    return get_combinations(A)
+end
+
+function apply_mask_v2(mem::Int, mask::AbstractString)
+    A, mask, out = collect(bitstring(mem)[(end - 35):end]), collect(mask), Vector{String}()
+    [setindex!(A, char, i) for (i, char) in enumerate(mask) if char ≠ '0']
+
+    return A
+end
+
+function get_possible_values(mem::Int, mask::Union{AbstractString, Vector{String}})
+    return get_combinations(apply_mask_v2(mem, mask))
+end
+
+function sum_values_remaining(datafile::String)
+    out, mask = Dict{Int, Int}(), string()
+
+    open(datafile) do io
+        while ! eof(io)
+            identifier, value = split(readline(io), " = ")
+
+            if identifier == "mask"
+                mask = value
+            else
+                mem_addr = parse(Int, match(r"\d+", identifier).match)
+                M = get_possible_values(mem_addr, mask)
+                key = parse(Int, value)
+
+                for m in M
+                    out[m] = key
+                end
+            end
+        end
+    end
+
+    return sum(values(out))
+end
+
+println(sum_values_remaining(datafile))
+
+#=
+BenchmarkTools.Trial:
+  memory estimate:  751.30 MiB
+  allocs estimate:  19955461
+  --------------
+  minimum time:     4.454 s (2.07% GC)
+  median time:      4.491 s (2.01% GC)
+  mean time:        4.491 s (2.01% GC)
+  maximum time:     4.528 s (1.95% GC)
+  --------------
+  samples:          2
+  evals/sample:     1
+=#
