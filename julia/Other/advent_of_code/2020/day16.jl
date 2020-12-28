277f654351dfaf5f1f2e7a746835d2a89d6f2e55
Jake W. Ireland <jakewilliami@icloud.com>
2020-12-29 12:27:50 +1300

    Finished day 16 (finally)

--- julia/Other/advent_of_code/2020/day16.jl
+++ julia/Other/advent_of_code/2020/day16.jl
@@ -37,12 +37,17 @@ function parse_input(datafile::String)
     return rules, my_ticket, nearby_tickets
 end
 
-function inranges(A::Vector{T}, D::Vector{R}) where {T <: Number} where R
-    return map(A -> any(R -> A ∈ R, Base.Iterators.flatten(D)), A)
+function inranges(A::Vector{T}, B::Vector{R}) where {T, R}
+    return map(a -> any(R -> a ∈ R, Iterators.flatten(B)), A)
 end
 
-function inranges(A::Vector{T}, D::Dict{R, S}) where {T <: Number} where {R, S}
-    return map(A -> any(R -> A ∈ R, Base.Iterators.flatten(values(D))), A)
+function inranges(A::Vector{T}, D::Dict{R, S}) where {T, R, S}
+    return map(a -> any(R -> a ∈ R, Iterators.flatten(values(D))), A)
+end
+
+function inranges(I::Base.ValueIterator{T}, R::Vector{S}) where {T, S}
+    println("We need to ensure that none of the ranges contain any of the vectors' values")
+    return map(r -> any(a -> a ∈ r, collect(I)), R)
 end
 
 function sum_invalid(
@@ -64,7 +69,6 @@ function sum_invalid(
 end
 
 println(sum_invalid(parse_input(datafile)...))
-# println(parse_input("inputs/test.txt"))
 
 #=
 
@@ -74,49 +78,48 @@ function getindices(A::Vector{Vector{T}}, i::Int) where {T, N}
     return T[a[i] for a in A]
 end
 
-function part2(
+function field_search(
     rules::Dict{String, Vector{UnitRange{Int}}},
     my_ticket::Vector{Int},
     nearby_tickets::Vector{Vector{Int}}
 )
+	
+	filtered_tickets, field_indices, len = copy(nearby_tickets), Dict{String, Int}(), length(rules)
 
-    filtered_tickets, field_indices = copy(nearby_tickets), Dict{String, Int}()
+	# filter the good tickets
+	for nearby_ticket in nearby_tickets
+		in_values_bools = inranges(nearby_ticket, rules)
+		I = findall(iszero, in_values_bools)
+		isempty(I) && continue
+		deleteat!(filtered_tickets, findfirst(e -> e == nearby_ticket, filtered_tickets))
+	end
 
-    for nearby_ticket in nearby_tickets
-        in_values_bools = inranges(nearby_ticket, rules)
-        I = findall(iszero, in_values_bools)
-        isempty(I) && continue
-        deleteat!(filtered_tickets, findfirst(e -> e == nearby_ticket, filtered_tickets))
-    end
-    
-    # for field in rules
-    for i in 1:length(first(filtered_tickets))
-        for field in rules
-        # for i in 1:length(first(filtered_tickets))
-            field_name, R = field
-            i ∈ values(field_indices) && break
-            println(keys(field_indices))
-            # field_name ∈ keys(field_indices) && break
-            # println("checking the $i'th position of nearby tickets $field_name's ranges $R")
-                
-            if all(inranges(getindices(filtered_tickets, i), R)) && R ∉ values(field_indices) && field_name ∉ keys(field_indices)
-                println("found")
-                field_indices[field_name] = i
-            end
-            
-            println(values(field_indices))
-            # i ∈ values(field_indices) && continue
-        end
-    end
-    
-    println(length(field_indices))
-    println(field_indices)
-    # println(prod(Int[my_ticket[idx] for (name, idx) in field_indices]))
-    return prod(Int[my_ticket[idx] for (name, idx) in field_indices if contains(name, "departure")])
+	positions = Vector{Int}[[e[i] for e in filtered_tickets] for i in 1:len]
+	mapped = Dict{String, Int}()
+	
+	# map ticket values to field names
+	while length(mapped) < len
+		for (i, pos) in enumerate(positions)
+			i ∈ values(mapped) && continue
+			valid = Vector{Tuple{String, Int}}()
+			
+			for rule in rules
+				field_name, R = rule
+				field_name ∈ keys(mapped) && continue
+				if all(inranges(getindices(filtered_tickets, i), R))
+					push!(valid, (field_name, i))
+				end
+			end
+			
+			if isone(length(valid))
+				i, rule = first(valid)
+				mapped[i] = rule
+			end
+		end
+	end
+	
+	# return product of relevant results
+	return prod(Int[my_ticket[idx] for (name, idx) in mapped if contains(name, "departure")])
 end
 
-# println(part2(parse_input("inputs/test2.txt")...))
-println(part2(parse_input(datafile)...))
-# println(part2(parse_input(datafile)...) > 465763613683) # we want this to be true
-
-# have guessed: 465763613683
+println(field_search(parse_input(datafile)...))
