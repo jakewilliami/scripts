874364de7fd3651ba6b14bba58ab52126e65e49d
Jake W. Ireland <jakewilliami@icloud.com>
2021-01-29 23:34:17 +1300

    More work on rust script

--- julia/Other/advent_of_code/2020/day17.jl
+++ julia/Other/advent_of_code/2020/day17.jl
@@ -10,6 +10,19 @@ n_adjacencies(I::AbstractIndexOrIndices{N}) where {N} = n_adjacencies(length(fir
 
 𝟘(n::T) where {T <: Integer} = ntuple(_ -> zero(T), n)
 
+function tryindex(M::AbstractArray{T, N}, idx::AbstractIndex{N}) where {T, N}
+    try
+        return getindex(M, idx...)
+    catch
+        return nothing
+    end
+    
+    # should never get here
+    return nothing
+end
+tryindex(M::AbstractArray{T, N}, inds) where {T, N} =
+    Union{T, Nothing}[tryindex(M, idx) for idx in inds]
+
 function get_directions(dim::T; include_zero::Bool = false) where T <: Integer
     D = reshape(collect(Iterators.product([-one(T):one(T) for _ in 1:dim]...)), :)
     return include_zero ? D : filter(d -> d ≠ 𝟘(dim), D)
@@ -54,7 +67,7 @@ function _expand_as_required(M::AbstractArray{T, N}, expand_by::T, inds::Abstrac
     indices = Vector{Union{T, Nothing}}()
     ind_extrema = extrema_indices(inds...)
     
-    for d in 1:ndims(M)
+    for d in 1:N
         ith_extrema = ind_extrema[d]
         if ! all(map(m -> m ≤ size(M, d), ith_extrema))
             for invalid_idx in filter(i -> i > size(M, i) || i < 1, ith_extrema)
@@ -74,16 +87,25 @@ expand_as_required(M::AbstractArray{T, N}, inds::AbstractIndex{N}...) where {T <
 
 # [println(i) for i in expand_as_required(reduce(vcat, permutedims(collect(i)) for i in readlines("inputs/test.txt")), '.', [(1, 1), (3, 3), (6, 6)])]
 
+# function adjacencies(M::Array{T, N}, expand_by::T, idx::AbstractIndex{N}) where {T, N}
+#     idx = Tuple(idx)
+#     direction_multipliers = get_directions(M, include_zero = false)
+#     adjacent_indices = [idx .+ j for j in direction_multipliers]
+#
+#     # println(tryindex(M, adjacent_indices))
+#     return filter(!isnothing, tryindex(M, adjacent_indices))
+#     # return
+# end
+
 function adjacencies(M::Array{T, N}, expand_by::T, idx::AbstractIndex{N}) where {T, N}
-    𝟎 = ntuple(_ -> zero(Int), N)
     idx = Tuple(idx)
     direction_multipliers = get_directions(M, include_zero = false)
     adjacent_indices = AbstractIndex{N}[idx .+ j for j in direction_multipliers]
     M = expand_as_required(M, expand_by, adjacent_indices)
     idx_shift = abs.(size(M) .- idx) # in case we have had to shift the array for non-positive indices
-    for i in adjacent_indices
-        println("We need to check the direction multiplier $i by shifting it with $(idx_shift .- 1)")
-    end
+    # for i in adjacent_indices
+    #     println("We need to check the direction multiplier $i by shifting it with $(idx_shift .- 1)")
+    # end
     return T[M[(i .+ idx_shift .- 1)...] for i in adjacent_indices]
 end
 adjacencies(M::Array{T, N}, idx::AbstractIndex{N}) where {T <: Number, N} = adjacencies(M, zero(T), idx)
@@ -106,27 +128,71 @@ function n_adjacent_to(M::AbstractArray{T, N}, expand_by::T, idx::AbstractIndex{
 end
 n_adjacent_to(M::AbstractArray{T, N}, idx::AbstractIndex{N}, adj_elem::T) where {T <: Number, N} = n_adjacent_to(M, zero(T), idx, adj_elem)
 
-function part1(layout::Vector{String})
+function iterate_slices(A::AbstractArray{T, N}) where {T, N}
+    return (A[(Colon() for _ in 1:(N - 1))..., i] for i in 1:N)
+end
+
+function part1(layout::Array{Char, N}) where N
     active, inactive = '#', '.'
-    layout = promote_to_3D(reduce(vcat, permutedims(collect(s)) for s in layout), inactive)
     layout_clone = copy(layout)
-    println(layout)
     
     for i in CartesianIndices(layout_clone)
-        println("checking $i")
-        n_active = n_adjacent_to(layout_clone, inactive, i, active)
+        # println("checking $i")
+        n_active = n_adjacent_to(layout_clone, inactive, i, active) # arr, expand_by, idx, adj_elem to count
         cube = layout_clone[i]
-        
-        if cube == active && n_active ∉ [2, 3]
-            # z_plane[y_idx] = inactive
-            println("found that $i does not have 2 or 3 actives surrounding it; changing to inactive")
-            layout[i] = inactive
-        elseif cube == inactive && n_active == 3
-            # z_plane[y_idx] = active
-            println("found that $i has 4 actives around it; changing it to active because it's a sheep")
-            layout[i] = active
+        # println("The current position $i has adjacencies $(adjacencies(layout, inactive, i))")
+        if cube == active
+            # If a cube is active and exactly 2 or 3 of its neighbors are also active,
+            # the cube remains active. Otherwise, the cube becomes inactive.
+            if !(n_active == 2 || n_active == 3)
+                println("found that $i does not have 2 or 3 actives surrounding it; changing to inactive")
+                layout[i] = inactive
+            end
+        elseif cube == inactive
+            # If a cube is inactive but exactly 3 of its neighbors are active,
+            # the cube becomes active. Otherwise, the cube remains inactive.
+            if n_active == 3
+                println("found that $i has 3 actives around it; changing it to active because it's a sheep")
+                layout[i] = active
+            end
         end
+    
     end
+    
+    # # Apparently we need to iterate slice by slice
+    # for (s_num, s) in enumerate(iterate_slices(layout))
+    #     # iterate through indices in each slice
+    #     for i in CartesianIndices(s)
+    #         n_active = n_adjacent_to(layout, inactive, i, active)
+    #         cube = layout[i]
+    #
+    #         if cube == active && n_active ∉ [2, 3]
+    #             # z_plane[y_idx] = inactive
+    #             # println("found that $i does not have 2 or 3 actives surrounding it; changing to inactive")
+    #             s[i] = inactive
+    #         elseif cube == inactive && n_active == 3
+    #             # z_plane[y_idx] = active
+    #             # println("found that $i has 4 actives around it; changing it to active because it's a sheep")
+    #             s[i] = active
+    #         end
+    #     end
+    #
+    #     layout[(Colon() for _ in 1:(N - 1))..., s_num] = s
+    # end
+    
+    
+    # assume mapsize is a vector with the desired shape, e.g. mapsize = [2,3,4]
+
+    # N = length(mapsize)
+    # Your line to create arrays below, with an extra initial dimension on each array
+    # arrays = [ (idx = ones(Int, N+1); idx[n+1] = i;reshape([1:i], tuple(idx...))) for (n,i) in enumerate(mapsize) ]
+
+    # Create indices and fill it one coordinate at a time
+    # indices = zeros(Int, tuple(N, mapsize...))
+    # for (i,arr) in enumerate(arrays)
+    #     dest = sub(indices, i, [Colon() for j=1:N]...)
+    #     broadcast!(identity, dest, arr)
+    # end
     
     # for z_idx in axes(layout_clone, 1)
     #     z_plane = layout_clone[z_idx, :, :]
@@ -146,22 +212,27 @@ function part1(layout::Vector{String})
     #     layout[z_idx, :] = row
     # end
     return layout
-    return permutedims(layout, [2, 3, 1])
+    # return permutedims(layout, [3, 1, 2])
     # return String[join(i) for i in eachrow(layout)]
 end
+part1(layout::Vector{String}) =
+    part1(promote_to_3D(reduce(vcat, permutedims(collect(s)) for s in layout), '.'))
+part1(datafile::String) =
+    part1(readlines(datafile))
 
-# function do_n_times(f::Function, n::Int, A::Vararg{T, N}) where {T, N}
-#     m = 0
-#
-#     while m < 6
-#         m += 1
-#         A = f(A)
-#         return do_n_times(f, n, A)
-#     end
-#
-#     return f(A...)
-# end
+function do_n_times(f::Function, n::Int, A::Vararg{T, N}) where {T, N}
+    m = 0
+    res = f(A...)
 
-# println(do_six_times(sum, [1, 2, 3]))
+    while m < n
+        m += 1
+        res = f(res)
+    end
 
-part1(readlines("inputs/test.txt"))
+    return res
+end
+
+# println(do_n_times(sum, 6, [1, 2, 3]))
+# do_n_times(part1, 6, readlines("inputs/test.txt"))
+
+part1("inputs/test.txt")
--- rust/gl/src/status.rs
+++ rust/gl/src/status.rs
@@ -1,10 +1,21 @@
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
+use std::ffi::OsString;
 
 static BASE_DIR: &str = "/Users/jakeireland/projects/";
 
-pub fn get_status(dir: &PathBuf) -> String {
-    let mut cmd = Command::new(format!("git -C {} status --short --branch", dir.as_path().display().to_string()));
+pub fn git_status(dir: &OsString) -> String {
+	// let mut os_string: OsString = "git -C ".into();
+    // os_string.push(&dir);
+    // os_string.push(" status --short --branch");
+    // let mut cmd = Command::new(os_string);
+	// println!("{:?}", cmd);
+	let mut cmd = Command::new("git");
+	cmd.arg("-C");
+	cmd.arg(dir);
+	cmd.arg("status");
+	cmd.arg("--short");
+	cmd.arg("--branch");
 	println!("{:?}", cmd);
 	let output = cmd
 		.stdout(Stdio::piped())
@@ -22,8 +33,39 @@ pub fn get_status(dir: &PathBuf) -> String {
 	}
 }
 
-pub fn parse_global() {
-    let input = std::fs::read_to_string(format!("{}/scripts/rust/gl/src/global.txt", BASE_DIR))
+pub fn git_diff_exit_code(dir: &OsString) {
+	// git diff-index --quiet HEAD --;
+	let mut cmd = Command::new("git");
+	cmd.arg("-C");
+	cmd.arg(dir);
+	cmd.arg("diff-index");
+	cmd.arg("--quiet");
+	cmd.arg("HEAD");
+	cmd.arg("--");
+	
+	let output = cmd
+		.stdout(Stdio::piped())
+		.output()
+		.expect("Failed to execute `git status`");
+		
+	
+	// return output.status;
+	
+	// if output.status.success() {
+	// 	let git_status = String::from_utf8_lossy(&output.stdout)
+	// 		.into_owned();
+	// 	// println!("{:?}", git_status);
+	// 	return git_status;
+	// } else {
+	// 	println!("An error has occured.  It is likely that you aren't in a git repository, or you may not have `git` installed.");
+	// 	return "".to_string();
+	// }
+}
+
+pub fn global_status() {
+	let mut input_file: OsString = BASE_DIR.into();
+	input_file.push("/scripts/rust/gl/src/global.txt");
+    let input = std::fs::read_to_string(input_file)
         .expect("Something went wrong reading the input file");
     let v: Vec<&str> = input
         .split('\n')
@@ -32,10 +74,51 @@ pub fn parse_global() {
 	
 	// let path: PathBuf = [r"C:\", "windows", "system32.dll"].iter().collect();
 	
+	// println!("{:?}", v);
+	
 	for r in v {
-		let constructed_path: &str = &format!("{}/{}/", BASE_DIR, r);
-		let git_repo: PathBuf = PathBuf::from(constructed_path);//.as_os_str();
+		
+		let mut constructed_path: OsString = BASE_DIR.into();
+		constructed_path.push("/");
+		constructed_path.push(r);
+		constructed_path.push("/");
+		// let git_repo: PathBuf = PathBuf::from(constructed_path);//.as_os_str();
+		// let git_repo: PathBuf = PathBuf::from(format!());//.as_os_str();
 		// println!("{:?}", git_repo);
-		println!("{:?}", get_status(&git_repo));
+		println!("{:?}", git_status(&constructed_path));
 	};
 }
+
+// 25 function parse_git_dirty {
+// 26     status=$(git status 2>&1 | tee)
+// 27     dirty=$(echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?")
+// 28     untracked=$(echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?")
+// 29     ahead=$(echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?")
+// 30     newfile=$(echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?")
+// 31     renamed=$(echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?")
+// 32     deleted=$(echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?")
+// 33     bits=''
+// 34     if [ "${renamed}" == "0" ]; then
+// 35         bits=">${bits}"
+// 36     fi
+// 37     if [ "${ahead}" == "0" ]; then
+// 38         bits="*${bits}"
+// 39     fi
+// 40     if [ "${newfile}" == "0" ]; then
+// 41         bits="+${bits}"
+// 42     fi
+// 43     if [ "${untracked}" == "0" ]; then
+// 44         bits="?${bits}"
+// 45     fi
+// 46     if [ "${deleted}" == "0" ]; then
+// 47         bits="x${bits}"
+// 48     fi
+// 49     if [ "${dirty}" == "0" ]; then
+// 50         bits="!${bits}"
+// 51     fi
+// 52     if [ ! "${bits}" == "" ]; then
+// 53         echo " ${bits}"
+// 54     else
+// 55         echo ""
+// 56     fi
+// 57 }
