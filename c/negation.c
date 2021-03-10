4e7b1e62ffd0d3989f8e04a38918a0c4878eb370
Jake W. Ireland <jakewilliami@icloud.com>
2021-03-10 13:14:23 +1300

    Added input method to negation script

--- c/negation.c
+++ c/negation.c
@@ -4,11 +4,21 @@
 // This is an interesting way of negating x (i.e., x becomes -x)
 // Something about bitwise and with negative zero?
 int main(int argc, char *argv[]) {
-	double x = 69.0;
-	double mz = -0.0;
-	uint64_t r = *(uint64_t*)&x ^ *(uint64_t*)&mz;
+/* int main(int argc, char** argv) */
+	// first element is invalid, so we start counting from one
+	for (int i = 1; i < argc; ++i){
+		// casting to double
+		/* double x = atof(argv[i]); */
+		/* double x = (double)argv[i]; */
+		double x = strtod(argv[i], NULL);
+		// do bitwise black magic
+		double mz = -0.0;
+		uint64_t r = *(uint64_t*)&x ^ *(uint64_t*)&mz;
+		
+		// print negation
+		printf("%lf\n", *(double*)&r);
+	}
 
-	printf("%lf\n", *(double*)&r);
-
+	// We need a return value
 	return 0;
 }
