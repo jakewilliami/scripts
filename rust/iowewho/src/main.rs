da6f92ddd4dfc575f02cdaf6b2ab8c644ab76a79
Jake W. Ireland <jakewilliami@icloud.com>
2021-04-28 11:35:47 +1200

    Added the beginnings of a script for working with financial data

---
+++ rust/iowewho/src/main.rs
@@ -0,0 +1,8 @@
+// This should be a programme to keep track of what I owe
+// Perhaps this reads from an Excel file
+// Perhaps I have my own CSV file
+// Perhaps I have a flag to list payees I owe money to, and a 'total' flag
+
+fn main() {
+    println!("Hello, world!");
+}
