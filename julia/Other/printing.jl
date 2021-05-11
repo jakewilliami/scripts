d6036caa8f5bd24ed1395944bf6478f63f9050c8
Jake W. Ireland <jakewilliami@icloud.com>
2021-05-11 20:16:58 +1200

    Finished fun project

--- julia/Other/printing.jl
+++ julia/Other/printing.jl
@@ -1,13 +1,15 @@
-function main(S::String)
+function first_attempt(S::String)
     str_odd_len = isodd(length(S))
     
     rev_S = reverse(S)
     left_diag = S[1:(length(S) ÷ 2)]
     left_diag = left_diag * reverse(left_diag)
-    right_diag = S[((length(S) ÷ 2) + (str_odd_len ? 2 : 1)):end] ^ 2
+    right_diag = S[((length(S) ÷ 2) + (str_odd_len ? 2 : 1)):end]
     right_diag = right_diag * reverse(right_diag)
 
-    
+    println(left_diag)
+    println(right_diag)
+
     out = String[S]
     println(S)
     for (i, j) in enumerate(2:(length(S) - 1))
@@ -22,16 +24,8 @@ function main(S::String)
             edge_spaces = " " ^ n_edge_spaces
             s = first_s * edge_spaces * S[length(S) ÷ 2] * edge_spaces * last_s
         else
-            n_edge_spaces = 0
-            # 1->0,2->1,3->4,4->4,5->3,6->2...
-            if i > (length(S) ÷ 2)
-                n_edge_spaces = mod((length(S) ÷ 2) - i, (length(S) ÷ 2)) - 2
-            else
-                n_edge_spaces = mod(i - 1, (length(S) ÷ 2))
-            end
+            n_edge_spaces = g(i, length(S)) - 1
             edge_spaces = " " ^ n_edge_spaces
-            # diag_s_start = left_diag[j]
-            # diag_s_end = right_diag[j]
             diag_s_start = S[j]
             diag_s_end = rev_S[j]
             n_middle_spaces = length(S) - 4 - (2 * n_edge_spaces)
@@ -47,45 +41,49 @@ function main(S::String)
     return out
 end
 
-main("Programm")
-println()
-println("""
-Programm
-rr    mm
-o o  a a
-g  gr  r
-r  rg  g
-a a  o o
-mm    rr
-mmargorP
-""")
+function construct_fun_crisscross(S::String)
+    function g(n::Integer, l::Integer)
+        rat = ((l + 1)//2)
+        res = rat - abs(n - rat)
+        res.den != 1 && error("Oh dear, we have a float; an unfortunate thing for an index to be")
+        return res.num
+    end
+    
+    str_odd_len = isodd(length(S))
+    
+    rev_S = reverse(S)
+    left_diag = S[1:(length(S) ÷ 2)]
+    left_diag = left_diag * reverse(left_diag)
+    right_diag = S[((length(S) ÷ 2) + (str_odd_len ? 2 : 1)):end]
+    right_diag = right_diag * reverse(right_diag)
+
+    out = String[S]
+
+    for (i, j) in enumerate(2:(length(S) - 1))
+        this_row = fill(' ', length(S))
+        
+        this_row[1] = S[j]
+        this_row[length(S)] = rev_S[j]
+        this_row[g(j, length(S))] = S[j]
+        this_row[length(S) - g(j, length(S)) + 1] = rev_S[j]
+        
+        push!(out, join(this_row))
+    end
+
+    push!(out, rev_S)
+
+    return out
+end
 
 
-# 2 -> 2
-# 3 -> 3
-# 4 -> 4
-# 5 -> 4
-# 6 -> 3
-# 7 -> 2
+function main()
+    for s in ARGS
+        for l in construct_fun_crisscross(s)
+            println(l)
+        end
 
+        println()
+    end
+end
 
-# for S in ARGS
-    # for l in main(S)
-        # println(l)
-    # end
-# end
-
-
-out2 = """
-Programming
-rr       nn
-o o     i i
-g  g   m  m
-r   r m   m
-a    a    a
-m   m r   r
-m  m   g  g
-i i     o o
-nn       rr
-gnimmargorP
-"""
+main()
