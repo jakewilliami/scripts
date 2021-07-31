0f97e1dce24711fc66f5a401845811a0d4eb0a8c
Jake W. Ireland <jakewilliami@icloud.com>
2021-08-01 01:27:46 +1200

    Created postcode interface

---
+++ rust/rohe/src/postcodes.rs
@@ -0,0 +1,40 @@
+#[derive(Debug)]
+pub struct Postcode {
+	pub postcode: String,
+}
+
+pub trait PostcodeConstructor {
+	fn parse_postcode(&self) -> Postcode;
+}
+
+impl PostcodeConstructor for i64 {
+	fn parse_postcode(&self) -> Postcode {
+		return Postcode {
+			postcode: format!("{:0>4}", &self)
+		};
+	}
+}
+
+impl PostcodeConstructor for i32 {
+	fn parse_postcode(&self) -> Postcode {
+		return Postcode {
+			postcode: format!("{:0>4}", &self)
+		};
+	}
+}
+
+impl PostcodeConstructor for &str {
+	fn parse_postcode(&self) -> Postcode {
+		return Postcode {
+			postcode: self.to_string()
+		};
+	}
+}
+
+impl PostcodeConstructor for String {
+	fn parse_postcode(&self) -> Postcode {
+		return Postcode {
+			postcode: self.to_string()
+		};
+	}
+}
