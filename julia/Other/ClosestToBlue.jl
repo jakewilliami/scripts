394ee7ca786e8b936c7785d33b6f631b4d415669
Jake W. Ireland <jakewilliami@icloud.com>
2021-08-31 12:03:41 +1200

    Performance considerations

--- julia/Other/ClosestToBlue.jl
+++ julia/Other/ClosestToBlue.jl
@@ -34,7 +34,11 @@ function calculate_colour_distance(C1::AbstractString, C2::AbstractRGB)
     return calculate_colour_distance(parse(RGB, C1), C2)
 end
 
+_rf_findmin((fm, im), (fx, ix)) = Base.isgreater(fm, fx) ? (fx, ix) : (fm, im)
+_argmin(f, domain) = mapfoldl(x -> (f(x), x), _rf_findmin, domain)[2]
+
 function closest_to_blue(colours::Vector{T}) where {T <: Union{AbstractRGB, AbstractString}}
-    return colours[last(findmin(Float64[calculate_colour_distance(BLUE, c) for c in colours]))]
+    return _argmin(c -> calculate_colour_distance(BLUE, c), colours)
+    # return colours[last(findmin(Float64[calculate_colour_distance(BLUE, c) for c in colours]))]
 end
 
