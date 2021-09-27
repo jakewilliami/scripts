3b4fa8a0c63c494788e17cd165d8e407b345c341
Jake Ireland <jakewilliami@icloud.com>
2021-09-27 13:54:41 +1300

    More assembly

--- c/asm/compile.sh
+++ c/asm/compile.sh
@@ -1,3 +1,5 @@
-nasm -felf64 "$1"
-ld -o "${1%%.*}" "${1%%.*}.o"
+set -xe
+nasm -f macho64 "$1"
+# nasm -felf64 "$1" # Linux
+ld -macosx_version_min 10.7.0 -lSystem -o "${1%%.*}" "${1%%.*}.o"
 rm "${1%%.*}.o"
--- c/asm/nothing.asm
+++ c/asm/nothing.asm
@@ -1,3 +1,7 @@
+;;; This value (defined using a macro) is defined by
+;;; the assembly gods.  Remember computers sometimes
+;;; work with registers
+%define SYS_EXIT 60
 ;;;  text segment is used to store the code
 ;;;  data segment used to store initialised variables
 ;;;  bss segment used to store uninitialised variables
@@ -5,5 +9,8 @@ segment .text
 ;;; _start is the entry point
 ;;; need to globally initialise it first
 global _start
-_start:	
+_start:
+	mov rax, SYS_EXIT
+	mov rdi, 0					; special error code 69
+	syscall
 	ret
