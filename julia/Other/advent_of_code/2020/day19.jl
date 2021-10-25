81a69cbf01df66354b18c643fa204f0e44c1fcda
Jake Ireland <jakewilliami@icloud.com>
2021-10-25 20:15:17 +1300

    Updated (slightly stolen) method to use the rule system that I started writing before getting some inspiration in solving the problem

--- julia/Other/advent_of_code/2020/day19.jl
+++ julia/Other/advent_of_code/2020/day19.jl
@@ -1,61 +1,87 @@
+const datafile = "inputs/data19.txt"
+
 using DataStructures
 
-Base.@kwdef struct SubRule
-    i::Int = 0
-    condition_indices::Vector{Tuple{Vararg{Int}}} = Tuple{Vararg{Int}}[]
+### Helper structs/type
+
+struct SubRule
+    i::Int
+    condition_indices::Vector{Tuple{Vararg{Int}}}
 end
 
-Base.@kwdef mutable struct Rule
-    i::Int = 0
-    # relies_on::Vector{SubRule} = SubRule[]
-    char_match::Union{Char, Nothing} = nothing
+struct Rule
+    i::Int
+    char_match::Char
 end
 
 AbstractRule = Union{Rule, SubRule}
 
-function parse_rules(rules_raw::Vector{String})
-    rules = AbstractRule[]
-    for rule_raw in rules_raw
-        local rule::Rule
-        idx_end_idx, rule_start_idx = findfirst(": ", rule_raw)
-        rule_idx = parse(Int, SubString(rule_raw, 1, prevind(rule_raw, idx_end_idx)))
-        rule_content = SubString(rule_raw, nextind(rule_raw, rule_start_idx))
-        if contains(rule_content, '"')
-            # then the rule is a direct rule, and we can push it directly
-            @assert length(rule_content) == 3 "Malformed direct rule; matches should only be a single character"
-            char_match = only(filter(!=('"'), rule_content))
-            rule = Rule(rule_idx, char_match)
-            push!(rules, rule)
+### Parse rules
+
+function parse_rule_rhs(rule_idx::Int, rule_rhs_raw::S)::AbstractRule where {S <: AbstractString}
+    @assert length(rule_rhs_raw) == 3 "Malformed direct rule; matches should only be a single character"
+    char_match = only(filter(!=('"'), rule_rhs_raw))
+    return Rule(rule_idx, char_match)
+end
+
+function parse_subrule_rhs(subrule_idx::Int, subrule_rhs_raw::S)::AbstractRule where {S <: AbstractString}
+    condition_indices = Tuple{Vararg{Int}}[]
+    condition_idx_tuples = split(subrule_rhs_raw, '|')
+    for condition_idx_tuple in condition_idx_tuples
+        condition_strs = split(condition_idx_tuple)
+        condition_idx_tuple = Tuple(parse(Int, condition_str) for condition_str in condition_strs)
+        push!(condition_indices, condition_idx_tuple)
+    end
+    return SubRule(subrule_idx, condition_indices)
+end
+
+function parse_rule(rule_raw::S) where {S <: AbstractString}
+    idx_end_idx, rule_start_idx = findfirst(": ", rule_raw)
+    rule_idx = parse(Int, SubString(rule_raw, 1, prevind(rule_raw, idx_end_idx)))
+    rule_content = SubString(rule_raw, nextind(rule_raw, rule_start_idx))
+    if contains(rule_content, '"')
+        # then the rule is a direct rule, and we can push it directly
+        return parse_rule_rhs(rule_idx, rule_content)
+    else
+        # then the rule is a subrule, which pertains to other abstract rules
+        return parse_subrule_rhs(rule_idx, rule_content)
+    end
+    error("Unreachable")
+end
+
+parse_rules(rules_raw::Vector{String}) =
+    AbstractRule[parse_rule(r) for r in rules_raw]
+
+findrule_index(j::Int, rules::Vector{AbstractRule}) = findfirst(r -> r.i == j, rules)
+function findrule(j::Int, rules::Vector{AbstractRule})
+    i = findrule_index(j, rules)
+    if isnothing(i)
+        throw(error(BoundsError, ": Cannot find rule with index $i"))
+    end
+    return rules[i]
+end
+
+function message_isvalid(s::S, rules::Vector{AbstractRule}; stack::Vector{Int} = zeros(Int, 1)) where {S <: AbstractString}
+    if isempty(s) || isempty(stack)
+        return isempty(s) && isempty(stack)
+    end
+
+    r = findrule(first(stack), rules)
+    if r isa Rule
+        if r.char_match == first(s)
+            return message_isvalid(SubString(s, nextind(s, 1)), rules, stack = stack[2:end]) # strip the first character
         else
-            condition_indices = Tuple{Vararg{Int}}[]
-            condition_idx_tuples = split(rule_content, '|')
-            for condition_idx_tuple in condition_idx_tuples
-                condition_strs = split(condition_idx_tuple)
-                condition_idx_tuple = Tuple(parse(Int, condition_str) for condition_str in condition_strs)
-                push!(condition_indices, condition_idx_tuple)
-            end
-            sub_rule = SubRule(rule_idx, condition_indices)
-            push!(rules, sub_rule)
+            return false # wrong first character
         end
+    elseif r isa SubRule
+        return any(message_isvalid(s, rules, stack = pushfirst!(stack[2:end], t...)) for t in r.condition_indices) # expand the first term
+    else
+        error("Unreachable")
     end
-    return rules
+    error("Unreachable")
 end
 
-findrule(j::Int, rules::Vector{AbstractRule}) = findfirst(r -> r.i == j, rules)
-
-function queue_rules(rules_original::Vector{AbstractRule})
-    rule_queue = Queue{AbstractRule}()
-    rules = deepcopy(rules_original)
-    local rule::AbstractRule
-    rule = deleteat!(rule, findrule(0, rules))
-    # enqueue!(rule_queue, first_rule)
-    while !isempty(rules)
-        # if rule isa Rule
-            # enqueue!()
-        # else
-    end
-    return rules
-end
+### Parse input
 
 function split_input(lines::Vector{String})
     rule_re = r"(\d+):\s(.*)"
@@ -71,61 +97,30 @@ function split_input(lines::Vector{String})
 end
 split_input(input_path::String) = split_input(readlines(input_path))
 
-# rules_raw, input_raw = split_input("inputs/test19b.txt")
-# rules = parse_rules(rules_raw)
-# rule_queue, queue_rules(rules)
-
-
-#######################################
-
-RuleDict = Dict{Int, Union{Vector{Vector{Int}}, AbstractString}}
-
-function message_isvalid(s::String, seq::Vector{Int}, rules::RuleDict)
-    if isempty(s) || isempty(seq)
-        return isempty(s) && isempty(seq)
-    end
-    
-    r = rules[seq[1]]
-    if '"' ∈ r
-        if contains(r, s[1])
-            return message_isvalid(s[2:end], seq[2:end], rules) # strip the first character
-        else
-            return false # wrong first character
-        end
-    else
-        return any(message_isvalid(s, vcat(t, seq[2:end]), rules) for t in r) # expand the first term
-    end
-end
-
-function parse_rule(s::String)
-    n, e = split(s, ": ")
-    if !contains(e, '"')
-        e = Vector{Int}[Int[parse(Int, r) for r in split(t)] for t in split(e, '|')]
-    end
-    return parse(Int, n), e
-end
-
 ### Part One
 
-part1(rules::RuleDict, messages::Vector{String}) =
-    sum(message_isvalid(m, zeros(Int, 1), rules) for m in messages)
+part1(rules::Vector{AbstractRule}, messages::Vector{String}) =
+    sum(message_isvalid(m, rules) for m in messages)
 function part1(input_path::String)
     rules_raw, messages = split_input(input_path)
-    rules = RuleDict(Pair(parse_rule(s)...) for s in rules_raw)
+    rules = parse_rules(rules_raw)
     return part1(rules, messages)
 end
 
 @assert part1("inputs/test19c.txt") == 2
 @assert part1("inputs/test19d.txt") == 3
-println("Part One: ", part1("inputs/data19.txt"))
+println("Part One: ", part1(datafile))
+
+### Part Two
 
 function part2(input_path::String)
     rules_raw, messages = split_input(input_path)
-    rules = RuleDict(Pair(parse_rule(s)...) for s in rules_raw)
-    rules[8] = last(parse_rule("8: 42 | 42 8"))
-    rules[11] = last(parse_rule("11: 42 31 | 42 11 31"))
+    rules = parse_rules(rules_raw)
+    rules[findrule_index(8, rules)] = parse_subrule_rhs(8, "42 | 42 8")
+    rules[findrule_index(11, rules)] = parse_subrule_rhs(11, "42 31 | 42 11 31")
+    # After altering the required rules, the rest of the problem is the same as part 1
     return part1(rules, messages)
 end
 
 @assert part2("inputs/test19d.txt") == 12
-println("Part Two: ", part2("inputs/data19.txt"))
+println("Part Two: ", part2(datafile))
