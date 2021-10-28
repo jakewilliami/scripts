77b89013773dccb166fbce7161d7fdf900faafff
Jake Ireland <jakewilliami@icloud.com>
2021-10-28 22:23:57 +1300

    First draft at implementation getting help from python code

--- julia/Other/advent_of_code/2020/day20.jl
+++ julia/Other/advent_of_code/2020/day20.jl
@@ -1,4 +1,5 @@
-using Combinatorics
+using DataStructures: OrderedDict
+using Combinatorics: permutations
 
 ### Helper structs
 
@@ -150,7 +151,105 @@ function part1(input_path::String)
     end
 end
 
-part1("inputs/test20a1.txt")
+# part1("inputs/test20a1.txt")
 
 # M = TilePermutation(reshape(Matrix{Char}[reduce(vcat, permutedims(collect(s)) for s in split(t)) for t in split(read("inputs/test20a2.txt", String), "\n\n")], 3, 3))
 # isvalid(M)
+
+##################################################################################################
+
+DEFAULT_M = 10
+
+function flip(n::Int, m::Int)
+    res = 0
+    for _ in 1:m
+        res <<= 1
+        if (n & 1) == 1
+            res |= 1
+        end
+        n >>= 1
+    end
+    return res
+end
+flip(n::Int) = flip(n, DEFAULT_M)
+
+edge_id(n::Int) = min(n, flip(n))
+
+
+
+function part1(tiles::Vector{<:AbstractString})
+    orig_tiles = Dict()
+    # edges = DefaultDict([])
+    edges = OrderedDict()
+    tilemap = Dict()
+    
+    for tile in tiles
+        tile_components = split(strip(tile), '\n')
+        header = tile_components[1]
+        img = tile_components[2:end]
+        
+        tile_id = parse(Int, split(header, ' ')[2][1:(end - 1)])
+        orig_tiles[tile_id] = img
+        
+        l, r = 0, 0
+        for line in img
+            l <<= 1
+            r <<= 1
+            l |= (line[1] == '#' ? 1 : 0) # |= Int(line[1] == '#')
+            r |= (line[end] == '#' ? 1 : 0)
+        end
+        
+        t, b = 0, 0
+        for i in 1:length(img[1])
+            t <<= 1
+            b <<= 1
+            t |= (img[1][i] == '#' ? 1 : 0)
+            b |= (img[end][i] == '#' ? 1 : 0)
+        end
+        
+        l, r, t, b = (edge_id(v) for v in (l, r, t, b)) # Tuple?
+        
+        # @info tile_id, string(l, base = 2), string(r, base = 2), string(t, base = 2), string(b, base = 2)
+        
+        tilemap[tile_id] = (t, l, b, r)
+        # push!(edges[l], tile_id)
+	    # push!(edges[r], tile_id)
+	    # push!(edges[t], tile_id)
+	    # push!(edges[b], tile_id)
+        edges[l] = push!(get(edges, l, []), tile_id)
+        edges[r] = push!(get(edges, r, []), tile_id)
+        edges[t] = push!(get(edges, t, []), tile_id)
+        edges[b] = push!(get(edges, b, []), tile_id)
+    end
+    
+    corners = []
+    init_edge = nothing
+    
+    for (tile_id, tile) in tilemap
+        match_cnt = 0
+        unused = -1
+        
+        for edge in tile
+            if length(edges[edge]) == 2
+                match_cnt += 1
+            elseif length(edges[edge]) == 1
+                unused = edge
+            end
+        end
+        
+        @assert match_cnt >= 2
+        
+        if match_cnt == 2
+            push!(corners, tile_id)
+            if isnothing(init_edge)
+                init_edge = unused
+            end
+        end
+    end
+    
+    
+    return prod(corners)
+end
+part1(filename::String) = part1(split(read(filename, String), "\n\n"))
+
+@assert part1("inputs/test20a1.txt") == 20899048083289
