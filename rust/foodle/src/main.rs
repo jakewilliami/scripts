8170ddc485cd443e86ed56694c4b27a0080a7462
Jake Ireland <jakewilliami@icloud.com>
2022-01-27 21:06:34 +1300

    Retroactive commit: (unfinished) wordle but for food

---
+++ rust/foodle/src/main.rs
@@ -0,0 +1,10 @@
+/*
+use trillium::Conn;
+use trillium_router::{Router, RouterConnExt};
+
+fn main() {
+    trillium_smol::run(|conn: Conn| async move {
+        conn.ok("hello from trillium!")
+    });
+}
+*/
