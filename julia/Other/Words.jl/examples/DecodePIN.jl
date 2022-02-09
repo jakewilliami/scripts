57c86ee1b0cee97dd3f1e7647a3449bd2cdc193f
Jake Ireland <jakewilliami@icloud.com>
2022-02-10 01:56:03 +1300

    Retroactive commit: Added script for decoding PIN

--- julia/Other/Words.jl/examples/DecodePIN.jl
+++ julia/Other/Words.jl/examples/DecodePIN.jl
@@ -11,6 +11,16 @@ const NUMPAD_CHARS = Vector{Char}[['\0'],                 # 0
                                   ['t', 'u', 'v'],        # 8
                                   ['w', 'x', 'y', 'z']]   # 9
 
+function string2pin(s::S) where {S <: AbstractString}
+    io = IOBuffer()
+    for c in s
+        i = findfirst(v -> c ∈ v, NUMPAD_CHARS)
+        @assert(!isnothing(i), "Cannot lower character \'$c\' to a digit on a regular num pad")
+        print(io, i - 1)
+    end
+    return String(take!(io))
+end
+
 possibile_char_permutations_lazy(numbers::Vector{Int}) = 
     foldr((itr1, itr2) -> ((v, w...) for w in itr2 for v in itr1), Vector{Char}[NUMPAD_CHARS[i + 1] for i in numbers])
 # TODO: make the following code work, as it is optimised over the `foldr` code:
@@ -39,8 +49,10 @@ end
 find_possible_words(pin::Int) = find_possible_words(reverse(digits(pin)))
 find_possible_words(pin::String) = find_possible_words(Int[parse(Int, c) for c in pin])
 
-println(find_possible_words(5253))
-println(find_possible_words(3345))
-println(find_possible_words(5683))
+# println(find_possible_words(5253))
+# println(find_possible_words(3345))
+# println(find_possible_words(5683))
+println(find_possible_words(2028))
 
 # Example: 5683 -> [..., "love", ...]
+println(string2pin("maze"))
