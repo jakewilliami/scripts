56001a620bb357214c334372e6e98ea92660658c
Jake Ireland <jakewilliami@icloud.com>
2022-02-18 08:29:37 +1300

    Moved regex to global

--- julia/Web/CommonAppVersions.jl/src/teamviewer.jl
+++ julia/Web/CommonAppVersions.jl/src/teamviewer.jl
@@ -1,5 +1,6 @@
 const TEAMVIEWER_URI = "https://www.teamviewer.com/en-us/download/windows/"
-# "mac-os", "windows"
+# "mac-os", "windows", "linux"
+const TEAMVIEWER_REGEX = r"Current version\: ((?:\d+\.)?(?:\d+\.)?(?:\d+))"  # Base.VERSION_REGEX
 
 struct TeamViewerSingleton <: CommonApplication end
 const TeamViewer = TeamViewerSingleton()
@@ -7,8 +8,7 @@ const TeamViewer = TeamViewerSingleton()
 function get_latest_version(::TeamViewerSingleton)
     r = HTTP.get(TEAMVIEWER_URI)
     body = String(r.body)
-    rx = r"Current version\: ((?:\d+\.)?(?:\d+\.)?(?:\d+))"  # Base.VERSION_REGEX
-    m = match(rx, body)
+    m = match(TEAMVIEWER_REGEX, body)
     v_str = only(m.captures)
     return VersionNumber(v_str)
     # doc = parsehtml(String(r.body))
