7d7b1e04d62f4f61637487d9068190ef53e852ae
Jake Ireland <jakewilliami@icloud.com>
2022-02-18 11:40:28 +1300

    Updated Firefox version (changed since last test\!)

--- julia/Web/CommonAppVersions.jl/test/runtests.jl
+++ julia/Web/CommonAppVersions.jl/test/runtests.jl
@@ -5,7 +5,7 @@ latest_version_equal(T::U, v::String) where {U <: CommonAppVersions.CommonApplic
     isequal(get_latest_version(T), VersionNumber(v))
 
 @testset "CommonAppVersions.jl" begin
-    @test latest_version_equal(Firefox, "97.0")
+    @test latest_version_equal(Firefox, "97.0.1")
     @test latest_version_equal(Chrome, "98.0.4758")
     @test latest_version_equal(TeamViewer, "15.25.8")
 end
