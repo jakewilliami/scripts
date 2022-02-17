1544159f25d26c72bdd9c72ad4b577d506fba35b
Jake Ireland <jakewilliami@icloud.com>
2022-02-18 10:02:02 +1300

    Added supported common applications dict

--- julia/Web/CommonAppVersions.jl/src/CommonAppVersions.jl
+++ julia/Web/CommonAppVersions.jl/src/CommonAppVersions.jl
@@ -5,6 +5,7 @@ using HTTP, Gumbo
 
 export get_latest_version
 export Firefox, Chrome, TeamViewer
+export COMMON_APPLICATIONS
 
 abstract type CommonApplication end
 
@@ -33,7 +34,21 @@ include("teamviewer.jl")
     get_latest_version(::CommonApplication)::VersionNumber
 
 Get latest app version for common applications.
+
+Supported common applications are in `COMMON_APPLICATIONS`.
 """
 function get_latest_version end
+
+const COMMON_APPLICATIONS = Dict{String, CommonApplication}(
+    "Mozilla Firefox" => Firefox,
+    "Google Chrome" => Chrome,
+    # "Adobe Acrobat DC" => Adobe,
+    "Team Viewer" => TeamViewer,
+    # "Microsoft Office 2007" => Office2007,
+    # "Microsoft Office 2010" => Office2010,
+    # "Microsoft Office 2013" => Office2013,
+    # "Microsoft Office 2016" => Office2016,
+    # "Microsoft Office 365" => Office365,
+)
 
 end
