80fc75b441ba947623943bfe143c2a284952e98b
Jake Ireland <jakewilliami@icloud.com>
2022-06-02 21:11:49 +1200

    Implement part separation for day 6 and benchmark

---
+++ julia/Other/advent_of_code/2021/day7/benchmark.md
@@ -0,0 +1,11 @@
+## Benchmarks
+
+| Command | Mean [ms] | Min [ms] | Max [ms] | Relative |
+|:---|---:|---:|---:|---:|
+| `./day7 1` | 50.8 ± 2.3 | 48.0 | 62.5 | 1.00 |
+| `./day7 2` | 12765.0 ± 194.3 | 12610.4 | 13244.7 | 251.08 ± 12.04 |
+| `julia ./day7.jl` | 666.4 ± 74.4 | 588.8 | 779.2 | 13.11 ± 1.58 |
+
+## Note
+
+I did nothing fancy for part two, which is why it's so slow.
--- julia/Other/advent_of_code/2021/day7/day7.rs
+++ julia/Other/advent_of_code/2021/day7/day7.rs
@@ -5,13 +5,20 @@ fn main() {
 	let crab_submarines = parse_input("data7.txt");
 	// let crab_submarines = parse_input("test.txt");
 	
-	// part 1
-	let part1_solution = part1(&crab_submarines);
-	println!("Part 1: {}", part1_solution);
+	let args: Vec<String> = std::env::args().collect();
+    let allowed_args: (String, String) = ("1".to_string(), "2".to_string());
+
+    // part 1
+    if *(&args.contains(&allowed_args.0)) {
+		let part1_solution = part1(&crab_submarines);
+		println!("Part 1: {}", part1_solution);
+	}
 	
 	// part 2
-	let part2_solution = part2(&crab_submarines);
-	println!("Part 2: {}", part2_solution);
+    if *(&args.contains(&allowed_args.1)) {
+		let part2_solution = part2(&crab_submarines);
+		println!("Part 2: {}", part2_solution);
+	}
 }
 
 // Structs and such
