c0d9cd25f17041f028347abf00092ad91ed081dd
Jake Ireland <jakewilliami@icloud.com>
2022-06-22 01:07:34 +1200

    Optimised findfirst_tuesday method

--- julia/Other/PenultimateTuesdays.jl
+++ julia/Other/PenultimateTuesdays.jl
@@ -2,8 +2,13 @@ using Dates
 
 _daysinmonth(m::Month, y::Year) = daysinmonth(y.value, m.value)
 
-findfirst_tuesday(m::Month, y::Year) = 
-    findfirst(i -> dayofweek(Date(Day(i), m, y)) == Tuesday, 1:_daysinmonth(m, y))
+# findfirst_tuesday(m::Month, y::Year) = 
+#     findfirst(i -> dayofweek(Date(Day(i), m, y)) == Tuesday, 1:_daysinmonth(m, y))
+function findfirst_tuesday(d::Date)
+    fd = firstdayofmonth(d)
+    return Day(fd).value + 7 - dayofweek(fd) + Tuesday
+end
+findfirst_tuesday(m::Month, y::Year) = findfirst_tuesday(Date(m, y))
 
 function penultimate_tuesday_date(m::Month, y::Year)
     ft = findfirst_tuesday(m, y)  # first Tuesday
@@ -17,3 +22,4 @@ function penultimate_tuesday_date(m::Month, y::Year)
 end
 penultimate_tuesday_date(d::Date) = penultimate_tuesday_date(Month(d), Year(d))
 
+# TODO: use above function to find the latest date which I can set a reminder for to find the penultimate Tuesday
