ede4391debe65e424c46c2239d2549e4498fc9a6
Jake Ireland <jakewilliami@icloud.com>
2022-11-08 22:24:54 +1300

    Make command-line interface for battery state script

--- rust/bats/src/main.rs
+++ rust/bats/src/main.rs
@@ -1,7 +1,37 @@
 use battery::{Battery, Manager};
 use battery::units::ratio::percent;
 
+use clap::{Parser, crate_version};
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "bats",
+    author = "Jake·W.·Ireland.·<jakewilliami@icloud.com>",
+    version = crate_version!(),
+    about = "A·simple command·line·interface·to display battery state",
+    long_about = None,
+)]
+struct Cli {
+    // /// Use ioreg to determine state of charge
+    // #[arg(short = 'i', long = "ioreg")]
+    // ioreg: Option<bool>,
+
+    // /// Use pmset to determine state of charge
+    // #[arg(short = 'p', long = "pmset")]
+    // pmset: Option<bool>,
+
+    // /// Use custom IOPS* Objective C-calls for macOS
+    // ///
+    // /// This method is almost certainly less efficient than the
+    // /// battery crate, from which the main method is implemented,
+    // /// however, I think it would be a fun test to implement
+    // #[arg(short = 'o', long = "objc")]
+    // objc: Option<bool>
+}
+
 fn main() {
+    let _args = Cli::parse();
+
     display_state_of_charge();
 }
 
