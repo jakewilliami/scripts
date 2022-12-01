f58c8790e15c7ec40c8af6a19a89a337f680280f
Jake Ireland <jakewilliami@icloud.com>
2022-12-01 21:09:43 +1300

    Use custom types in leaderboard parsing

--- julia/Other/advent_of_code/2022/parse_leaderboard.jl
+++ julia/Other/advent_of_code/2022/parse_leaderboard.jl
@@ -2,27 +2,109 @@ using Dates
 
 using JSON
 
-# julia --project=. parse_leaderboard.jl --day=1 '<JSON DATA>'
+# Usage:
+#   julia --project=. parse_leaderboard.jl --day=1 '<JSON DATA>'
+
+
+# Statistics structs
+
+struct StarStats
+    n::Int
+    mins::Float64
+end
+
+struct DayStats
+    day::Int
+    stats::Vector{StarStats}
+end
+
+struct UserStats
+    name::String
+    stats::Vector{DayStats}
+end
+
+
+# Minimal initialisation methods
+
+DayStats(day::Int) = DayStats(day, StarStats[])
+UserStats(name::String) = UserStats(name, DayStats[])
+
+
+# Helper functions
 
 function get_unix_time_since_day_start(dᵢ::Int, ts::Int)
     d = DateTime(2022, 12, dᵢ) + Hour(5)  # Account for EST/UTC–5
     return (ts - datetime2unix(d)) / 60
 end
 
-function main(json_data::String)
+
+function sort_stats!(stats::Vector{UserStats})
+    sort!(stats, by = s -> sum(d.stats[2].mins for d in s.stats))
+    return stats
+end
+
+
+# Parse function
+
+function parse_user_stats(json_data::String)
+    # Initialise results
+    user_stats = UserStats[]
+
+    # Iterate over users
     for (_user_id, data) in JSON.parse(json_data)["members"]
+        # If they have no completed days, skip user
         isempty(data["completion_day_level"]) && continue
-        println(data["name"], ':')
+
+        this_user_stats = UserStats(data["name"])
+
         for dᵢ in 1:25
             dᵢˢ = string(dᵢ)
+
+            # Skip day if haven't done yet
             haskey(data["completion_day_level"], dᵢˢ) || continue
-            println("    Day ", lpad(dᵢˢ, 2), ':')
+
+            day_stats = DayStats(dᵢ)
             for sᵢ in 1:2
                 sᵢˢ = string(sᵢ)
+
+                # Skip part/level/star if incomplete for this day
                 haskey(data["completion_day_level"][dᵢˢ], sᵢˢ) || continue
+
+                # Calculate minutes since problem released
                 ts = data["completion_day_level"][dᵢˢ][sᵢˢ]["get_star_ts"]
                 m = get_unix_time_since_day_start(dᵢ, ts)
-                println("        ", lpad(sᵢˢ, 2), ": ", m, " mins")
+
+                # Add star stats to day stats
+                star_stats = StarStats(sᵢ, m)
+                push!(day_stats.stats, star_stats)
+            end
+
+            # Add day stats to user stats
+            push!(this_user_stats.stats, day_stats)
+        end
+
+        # Add user stats to results
+        push!(user_stats, this_user_stats)
+    end
+
+    return user_stats
+end
+
+
+# Main function
+
+function main(json_data::String)
+    # Parse statistics
+    stats = parse_user_stats(json_data)
+    sort_stats!(stats)
+
+    # Display results
+    for user in stats
+        println(user.name, ':')
+        for day in user.stats
+            println("    Day ", lpad(day.day, 2), ':')
+            for star in day.stats
+                println("    ", lpad(star.n, 2), ": ", star.mins, " mins")
             end
         end
     end
