19c1e1c65137638684f9d471c7876e8a87a16133
Jake Ireland <jakewilliami@icloud.com>
2022-12-03 00:01:18 +1300

    Clean up day 2 Julia solution for AoC 2022

--- julia/Other/advent_of_code/2022/day02/day02.jl
+++ julia/Other/advent_of_code/2022/day02/day02.jl
@@ -2,98 +2,55 @@ const DATA_FILE = "data02.txt"
 
 parse_input(f::String) = Pair{Char, Char}[line[1] => line[end] for line in readlines(f)]
 
+@enum Play Rock=1 Paper Scissors
+@enum Outcome Lose=0 Win=6 Draw=3
+
+const COL1_MAP = Dict{Char, Play}('A' => Rock, 'B' => Paper, 'C' => Scissors)
 
 # Part 1
 
-@enum Play Rock=1 Paper=2 Scissors=3
-
-const COL1_MAP = Dict{Char, Play}('A' => Rock, 'B' => Paper, 'C' => Scissors)
 const COL2_MAP_1 = Dict{Char, Play}('X' => Rock, 'Y' => Paper, 'Z' => Scissors)
 
 parse_input_1(data::Vector{Pair{Char, Char}}) = Pair{Play, Play}[COL1_MAP[a] => COL2_MAP_1[b] for (a, b) in data]
 
+# Does play b beat play a?
 function wins(a::Play, b::Play)
-    a == b && return 2
-    v = [Paper, Scissors, Rock]
-    if (a == Rock && b == Paper)
-        return 1
-    elseif a == Rock && b == Scissors
-        return 0
-    elseif a == Paper && b == Scissors
-        return 1
-    elseif a == Scissors && b == Paper
-        return 0
-    elseif a == Paper && b == Rock
-        return 0
-    elseif a == Scissors && b == Rock
-        return 1
-    elseif a == Rock && b == Paper
-        return 0
-    elseif a == b
-        return 2
-    end
-    error("unreachable: $a, $b")
+    a == b && return Draw
+    won = Play(mod1(Int(a) + 1, 3)) == b
+    return won ? Win : Lose
 end
 
-function play(input::Vector{Pair{Play, Play}})
-    res = 0
-    for i in input
-        w = wins(i...)
-        res += iszero(w) ? 0 : Int(6 / w)
-        res += Int(i[2])
-    end
-    return res
-end
+play(input::Union{Vector{Pair{Play, Play}}, Vector{Pair{Play, Outcome}}}) =
+    sum(Int(b) + Int(wins(a, b)) for (a, b) in input)
 
 
 # Part 2
-
-@enum Outcome Lose=0 Win=6 Draw=3
 
 const COL2_MAP_2 = Dict{Char, Outcome}('X' => Lose, 'Y' => Draw, 'Z' => Win)
 
 parse_input_2(data::Vector{Pair{Char, Char}}) = Pair{Play, Outcome}[COL1_MAP[a] => COL2_MAP_2[b] for (a, b) in data]
 
 function wins(a::Play, b::Outcome)
-    v = [Paper, Scissors, Rock]
-    v′ = circshift(v, -1)
-    i = Int(a)
-    if b == Win
-        return v[i]
-    elseif b == Lose
-        return v′[i]
-    elseif b == Draw
-        return a
-    end
-    error("unreachable: $a, $b")
-end
-
-function play(input::Vector{Pair{Play, Outcome}})
-    res = 0
-    for i in input
-        res += Int(wins(i...))
-        res += Int(i[2])
-    end
-    return res
+    b == Draw && return a
+    return Play(b == Win ? mod1(Int(a) + 1, 3) : mod1(Int(a) - 1, 3))
 end
 
 
 # Main
 
 function main()
-    # 1 = 10595
-    # 2 = 9541
-
     raw_data = parse_input("data02.txt")
 
     # Part 1
     part1_data = parse_input_1(raw_data)
     part1_solution = play(part1_data)
+    @assert part1_solution == 10595
     println("Part 1: $part1_solution")
 
     # Part 2
     part2_data = parse_input_2(raw_data)
     part2_solution = play(part2_data)
+    @assert part2_solution == 9541
     println("Part 2: $part2_solution")
 end
 
