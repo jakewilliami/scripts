79f7ada95ecf200afcff92823d6be3bbd2d72c0b
Jake Ireland <jakewilliami@icloud.com>
2022-12-03 00:22:43 +1300

    Further specify imports for Rust leaderboard project

--- julia/Other/advent_of_code/2022/leaderboard/winphysec/src/main.rs
+++ julia/Other/advent_of_code/2022/leaderboard/winphysec/src/main.rs
@@ -2,9 +2,16 @@ use std::env;
 
 use dotenv::dotenv;
 
+
 mod dates;
+use dates::format_time_summary;
+
 mod request;
+use request::pull_leaderboard_data;
+
 mod stats;
+use stats::parse_user_stats;
+
 
 #[tokio::main]
 async fn main() {
@@ -13,15 +20,15 @@ async fn main() {
     let leaderboard_id = env::var("LEADERBOARD_ID").expect("Could not find \"LEADERBOARD_ID\" in .env");
     let session_cookie = env::var("SESSION_COOKIE").expect("Could not find \"SESSION_COOKIE\" in .env");
 
-    let res = request::pull_leaderboard_data(leaderboard_id, session_cookie).await;
-    let user_stats = stats::parse_user_stats(res);
+    let res = pull_leaderboard_data(leaderboard_id, session_cookie).await;
+    let user_stats = parse_user_stats(res);
 
     for user in user_stats {
         println!("{}:", user.name);
         for day in user.stats {
             println!("    Day {}:", day.day);
             for star in day.stats {
-                println!("        {}: {}", star.n, dates::format_time_summary(star.time_summary));
+                println!("        {}: {}", star.n, format_time_summary(star.time_summary));
             }
         }
     }
