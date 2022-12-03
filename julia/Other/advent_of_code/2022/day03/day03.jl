13e8d913c1c5d6edd81b007097ee26c8f76ce0d6
Jake Ireland <jakewilliami@icloud.com>
2022-12-03 18:30:05 +1300

    Clean up AoC Day 3 Julia solution

--- julia/Other/advent_of_code/2022/day03/day03.jl
+++ julia/Other/advent_of_code/2022/day03/day03.jl
@@ -1,26 +1,23 @@
 parse_input(data_file::String) =
-    [line for line in readlines("data03.txt")]
+    String[line for line in readlines(data_file)]
 
-priority(c::Char) = 'a' ≤ c ≤ 'z' ? Int(c) - 96 : Int(c) - 64 + 26
+function priority(c::Char)
+    if 'a' ≤ c ≤ 'z'
+        return Int(c) - 96
+    elseif 'A' ≤ c ≤ 'Z'
+        return Int(c) - 64 + 26
+    end
+    error("unreachable: $c")
+end
 
 
 # Part 1
-
-function common_items(a, b)
-    v = []
-    for c in a
-        if c ∈ b
-            push!(v, c)
-        end
-    end
-    return unique(v)
-end
 
 function part1(data::Vector{String})
     res = 0
     for r in data
         a, b = r[1:(length(r) ÷ 2)], r[((length(r) ÷ 2) + 1):end]
-        v = common_items(a, b)
+        v = intersect(a, b)
         for c in v
             res += priority(c)
         end
@@ -31,20 +28,11 @@ end
 
 # Part 2
 
-function common_badge(v)
-    for i in 1:length(v)
-        for c in v[i]
-            if c ∈ v[mod1(i + 1, 3)] && c ∈ v[mod1(i + 2, 3)]
-                return c
-            end
-        end
-    end
-end
-
 function part2(data::Vector{String})
     res = 0
     for v in Base.Iterators.partition(data, 3)
-        res += priority(common_badge(v))
+        common = only(intersect(v...))
+        res += priority(common)
     end
     return res
 end
